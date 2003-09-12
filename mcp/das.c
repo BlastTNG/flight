afc79c7a16f09581e8c6f4ba84ad58729615acb3
Don Wiebe <dvw@ketiltrout.net>
2003-09-12 11:07:19 +0000

    Callamp

--- mcp/das.c
+++ mcp/das.c
@@ -81,7 +81,7 @@ int CalLamp (int* cryostate) {
     if (repeat_left == 0) {
       if (CommandData.Cryo.calib_repeat > 0 && /* we're repeating and */
           CommandData.Cryo.calib_pulse > 0) {  /* just got to the end */
-        repeat_left = CommandData.Cryo.calib_repeat * 100;
+        repeat_left = CommandData.Cryo.calib_repeat * 100 - 1;
         pulse_left = CommandData.Cryo.calib_pulse + 1;
       }
     } else {  /* we're repeat pulsing, and counting down to next pulse */
