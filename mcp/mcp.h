a6b9e4ce52a69702d5c8462cd4e14d72073a6c4a
Don Wiebe <dvw@ketiltrout.net>
2004-01-16 17:50:53 +0000

    mprintf class functions header

---
+++ mcp/mcp.h
@@ -0,0 +1,16 @@
+#ifndef MCP_H
+#define MCP_H
+
+#define MCP_INFO    0
+#define MCP_STARTUP 1
+#define MCP_SCHED   2
+#define MCP_WARNING 3
+#define MCP_ERROR   4
+#define MCP_TFATAL  5
+#define MCP_FATAL   6
+
+void mprintf(int flag, char* fmt, ...);
+void merror(int flag, char* fmt, ...);
+void mputs(int flag, char* message);
+
+#endif
