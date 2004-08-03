c8d8551b259161c1721c44da3c1d680b6674ef2b
Enzo Pascale <enzo.pascale@astro.cf.ac.uk>
2004-08-03 23:18:29 +0000

    *** empty log message ***

--- bbcpci/bbc_pci.c
+++ bbcpci/bbc_pci.c
@@ -239,16 +239,16 @@ static ssize_t bbc_write(struct file *filp, const char __user *buf,
     if(to_write == 0) return 0;
 
     for(i = 0; i < to_write; i++) {
-      while(bi0_wfifo.n == BI0_WFIFO_SIZE) {
+/*      while(bi0_wfifo.n == BI0_WFIFO_SIZE) {
         udelay(1000);
         //sleep_on_timeout(&bi0_wq, jiffies+1);
-      }
+      }*/
       
-      //if(bi0_wfifo.n == BI0_WFIFO_SIZE) {
-	  //printk(KERN_WARNING "%s: bi0 buffer overrun. size = %x\n", 
-	  //     DRV_NAME, bi0_wfifo.n);
-	  //break;
-      //}
+      if(bi0_wfifo.n == BI0_WFIFO_SIZE) {
+	    printk(KERN_WARNING "%s: bi0 buffer overrun. size = %x\n", 
+	       DRV_NAME, bi0_wfifo.n);
+	    break;
+      }
       
       copy_from_user( (void *)&bi0_wfifo.data[bi0_wfifo.i_in],
 		      in_buf, sizeof(unsigned short) );
