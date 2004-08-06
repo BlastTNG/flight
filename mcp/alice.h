eb3c937e77af16cc08fb827bb26cd0bf315a2328
Adam Hincks <ahincks@phas.ubc.ca>
2004-08-06 20:29:29 +0000

    Updated alice.h.

--- mcp/alice.h
+++ mcp/alice.h
@@ -5,12 +5,20 @@
 |*                                                                            *|
 \******************************************************************************/
 
+// Compression types.
+#define COMP_DIFFERENTIAL       0
+#define COMP_INT_PRESERVING     1
+#define COMP_SINGLE             2
+#define COMP_AVERAGE            3
+
+// Special bytes.
 #define BUF_FRAME_SYNC          0xff
 #define BUF_FILE_NUM_PADDER     0xf0
 #define BUF_SECTION_SYNC        0xaa
 #define BUF_NO_DATA             0x99
 #define BUF_END_SYNC            0x55
 
+// Reserved bytes in the frame.
 #define BUF_POS_FRAME_SYNC      0
 #define BUF_LEN_FRAME_SYNC      4
 #define BUF_POS_FILE_NUM        (BUF_POS_FRAME_SYNC + BUF_LEN_FRAME_SYNC)
@@ -20,4 +28,5 @@
 #define BUF_POS_CRC             (BUF_POS_FRAME_LEN + 2)
 #define BUF_POS_DATA_START      (BUF_POS_CRC + 2)
 
+// CRC stuff.
 #define CRC_INIT                0
