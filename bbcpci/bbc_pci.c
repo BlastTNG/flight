f239f733aa1e43121aa7970dc38aa0420889951f
Don Wiebe <dvw@ketiltrout.net>
2004-09-08 23:39:32 +0000

    license

--- bbcpci/bbc_pci.c
+++ bbcpci/bbc_pci.c
@@ -1,3 +1,25 @@
+/* bbc_pci: kernel driver for the PCI BLAST Bus Controller
+ *
+ * This software is copyright (C) 2004 University of Toronto
+ * 
+ * This file is part of bbc_pci.
+ * 
+ * bbc_pci is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * bbc_pci is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with bbc_pci; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 #include <linux/version.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
