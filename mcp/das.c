ee20ac67724b44ec3c13e69a5a331d1621a7b0c1
Don Wiebe <dvw@ketiltrout.net>
2004-12-09 17:41:58 +0000

    fprintf -> bprintf

--- mcp/das.c
+++ mcp/das.c
@@ -480,12 +480,12 @@ void BiasControl (unsigned short* RxFrame) {
   if (isBiasAC) { /*  Bias is currently AC */
     if (CommandData.Bias.biasAC == 0) { /* it should be DC */
       biasout1 |= 0x01;
-      /*      fprintf(stderr, "to DC\n"); */
+      /*      bprintf(info, "to DC\n"); */
     }
   } else { /* Bias is currently DC */
     if (CommandData.Bias.biasAC == 1) { /* it should be AC */
       biasout1 |= 0x02;
-      /*      fprintf(stderr, "to AC\n"); */
+      /*      bprintf(info, "to AC\n"); */
     }
   }
 
@@ -493,12 +493,12 @@ void BiasControl (unsigned short* RxFrame) {
   if (isBiasRamp) { /* Bias is currently external Ramp */
     if (CommandData.Bias.biasRamp == 0) { /* it should be internal/fixed */
       biasout1 |= 0x40;
-      /*      fprintf(stderr, "to fixed\n"); */
+      /*      bprintf(info, "to fixed\n"); */
     }
   } else { /* Bias is currently internal (fixed) */
     if (CommandData.Bias.biasRamp == 1) { /* it should be external/Ramp */
       biasout1 |= 0x80;
-      /*      fprintf(stderr, "to ramp\n"); */
+      /*      bprintf(info, "to ramp\n"); */
     }
   }
 
@@ -506,12 +506,12 @@ void BiasControl (unsigned short* RxFrame) {
   if (isBiasClockInternal) { /* Bias is currently internal */
     if (CommandData.Bias.clockInternal == 0) { /* it should be external */
       biasout1 |= 0x10;
-      /*      fprintf(stderr, "to external\n"); */
+      /*      bprintf(info, "to external\n"); */
     }
   } else { /* Bias clock is currenly external */
     if (CommandData.Bias.clockInternal == 1) { /* it should be internal */
       biasout1 |= 0x20;
-      /*      fprintf(stderr, "to internal\n"); */
+      /*      bprintf(info, "to internal\n"); */
     }
   }
 
