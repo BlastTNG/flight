0572cff241b595db0ff56d5b1e8f94210ae6ee45
Don Wiebe <dvw@ketiltrout.net>
2005-01-20 14:42:52 +0000

    autoconf version checking

--- blastcmd/bootstrap.sh
+++ blastcmd/bootstrap.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-aclocal
-automake --add-missing
-autoconf configure.ac > configure
-chmod 755 configure
-autoheader configure.ac > config.h.in
+set -x
+
+if autoconf --version | grep -q 2.13; then
+  rm -f configure.in
+  sed s/AC_INIT\(.*\)/AC_INIT/ configure.ac > configure.in
+  mv configure.ac configure.old
+  aclocal 
+  automake --add-missing
+  autoconf
+  autoheader
+  mv configure.old configure.ac
+else
+  aclocal 
+  automake --add-missing
+  autoconf
+  autoheader
+fi
