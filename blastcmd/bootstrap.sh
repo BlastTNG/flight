88d85ab0a460a7066f23056b00fdc37918f87031
Don Wiebe <dvw@ketiltrout.net>
2005-01-22 02:15:15 +0000

    Add bootstrapper

--- blastcmd/bootstrap.sh
+++ blastcmd/bootstrap.sh
@@ -1,25 +1,32 @@
 #!/bin/sh
 
+FINISHED_OK=0
 set -x
 
 if autoconf --version | grep -q 2.13; then
-  rm -f configure.in
-  sed s/AC_INIT\(.*\)/AC_INIT/ configure.ac > configure.in
-  mv configure.ac configure.old
-  aclocal 
-  autoheader
-  automake --add-missing
-  autoconf
-  mv configure.old configure.ac
+  rm -f configure.in &&
+  sed s/AC_INIT\(.*\)/AC_INIT/ configure.ac > configure.in &&
+  mv configure.ac configure.old &&
+  aclocal &&
+  autoheader &&
+  automake --add-missing &&
+  autoconf &&
+  mv configure.old configure.ac &&
+  FINISHED_OK=1
 else
-  aclocal 
-  autoheader
-  automake --add-missing
-  autoconf
+  aclocal &&
+  autoheader &&
+  automake --add-missing &&
+  autoconf &&
+  FINISHED_OK=1
 fi
 
 set +x
 
 echo ""
-echo "good, bootstrap finished.  Now try ./configure"
+if [ "$FINISHED_OK" == 1 ]; then
+  echo "good: bootstrap finished.  Now try ./configure"
+else
+  echo "oops: bootstrapping errors.  Check your package or try \`cvs update'"
+fi
 echo ""
