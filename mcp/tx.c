de8badfaee063a6fb3954934ba8ad3f73b396af5
Don Wiebe <dvw@ketiltrout.net>
2005-05-11 19:51:10 +0000

    fix calib

--- mcp/tx.c
+++ mcp/tx.c
@@ -746,9 +746,9 @@ void StoreData(int index)
   WriteData(elAddr, (unsigned int)(PointingData[i_point].el * 65536.0/360.0),
       NIOS_QUEUE);
 
-  WriteData(raAddr, (unsigned int)(PointingData[i_point].ra * 65536.0/24.0),
-      NIOS_QUEUE);
-  WriteData(decAddr, (unsigned int)(PointingData[i_point].dec * 65536.0/360.0),
+  WriteData(raAddr, (unsigned int)(PointingData[i_point].ra * (65536.0 *
+          65536) / 24.0), NIOS_QUEUE);
+  WriteData(decAddr, (unsigned int)(PointingData[i_point].dec * DEG2LI),
       NIOS_QUEUE);
 
   WriteData(gy1OffsetAddr,
