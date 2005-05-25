3be3ea50559a5fd1c4684085b311c9b2b8c8ae04
Don Wiebe <dvw@ketiltrout.net>
2005-05-25 18:39:34 +0000

    Remove messages.

--- mcp/commands.c
+++ mcp/commands.c
@@ -1283,17 +1283,17 @@ void WatchPort (void* parameter) {
       if (timer == 800) {
         pthread_mutex_lock(&mutex);
         SendRequest (REQ_POSITION, tty_fd);
-        bprintf(info, "Commands: COMM%i: Request SIP Position\n", port + 1);
+//        bprintf(info, "Commands: COMM%i: Request SIP Position\n", port + 1);
         pthread_mutex_unlock(&mutex);
       } else if (timer == 1700) {
         pthread_mutex_lock(&mutex);
         SendRequest (REQ_TIME, tty_fd);
-        bprintf(info, "Commands: COMM%i: Request SIP Time\n", port + 1);
+//        bprintf(info, "Commands: COMM%i: Request SIP Time\n", port + 1);
         pthread_mutex_unlock(&mutex);	
       } else if (timer > 2500) { 
         pthread_mutex_lock(&mutex);
         SendRequest (REQ_ALTITUDE, tty_fd);
-        bprintf(info, "Commands: COMM%i: Request SIP Altitude\n", port + 1);
+//        bprintf(info, "Commands: COMM%i: Request SIP Altitude\n", port + 1);
         pthread_mutex_unlock(&mutex);
         timer = 0;
       }
