1df4674bb3949c1d50ae3e74c03b98bffc8ee83f
Don Wiebe <dvw@ketiltrout.net>
2006-03-20 23:16:22 +0000

    Merge netisc isc_protocol.h with mcp's (mostly whitespace correction).

--- blastcmd/isc_protocol.h
+++ blastcmd/isc_protocol.h
@@ -1,4 +1,5 @@
-/*  isc_protocol.h: define data structures for communcation between the server (ISC) and clients (Sam,Frodo,...)
+/*  isc_protocol.h: define data structures for communcation between the
+ *  server (ISC) and clients (Sam,Frodo,...)
  *
  * This software is copyright (C) 2003-2005 Edward Chapin
  *
@@ -12,108 +13,113 @@
  */
 
 /*
-	Overview:
+  Overview:
 
-	The server program (netisc on the ISC computer) will connect several clients, each on a separate port.
+    The server program (netisc on the ISC computer) will connect
+    several clients, each on a separate port.
 
-	Any client may send ISCStatusStruct data frames. The server takes the appropriate action to achieve that status.
-	While the camera is busy changing the status, the server will continue accepting new frames but will ignore
-	them until the previous changes have been made and it is ready to make further changes.
+    Any client may send ISCStatusStruct data frames. The server takes
+    the appropriate action to achieve that status.  While the camera
+    is busy changing the status, the server will continue accepting
+    new frames but will ignore them until the previous changes have
+    been made and it is ready to make further changes.
 
-	The server sends ISCSolutionStruct data frames to all connected clients whenever a new image has been processed.
+    The server sends ISCSolutionStruct data frames to all connected
+    clients whenever a new image has been processed.
 
 */
 
 #ifndef __ISC_PROTOCOL_H
 #define __ISC_PROTOCOL_H
 
-#ifdef _WINDOWS_	// If we're in windows need to change the packing order of the frames so that they have the
-#pragma pack(2)		// same packing as in Linux
+#ifdef _WINDOWS_  // If in windows change packing order of frames to have
+#pragma pack(2)    // same packing as Linux
 #endif
 
-#define MAX_ISC_BLOBS 20					// static # blobs to store in data frames
+#define MAX_ISC_BLOBS 20      // static # blobs to store in data frames
 
-#define ISC_CCD_X_PIXELS 1312				// pixel dimenions of the ISC CCD
-#define ISC_CCD_Y_PIXELS 1024				//   "        "     "   "   "
+#define ISC_CCD_X_PIXELS 1312 // pixel dimenions of the ISC CCD
+#define ISC_CCD_Y_PIXELS 1024 //   "        "     "   "   "
 
-#define OSC_CCD_X_PIXELS 1360				// pixel dimenions of the OSC CCD
-#define OSC_CCD_Y_PIXELS 1036				//   "        "     "   "   "
+#define OSC_CCD_X_PIXELS 1360 // pixel dimenions of the OSC CCD
+#define OSC_CCD_Y_PIXELS 1036 //   "        "     "   "   "
 
-#define FOCUS_RANGE 2550					// # steps range for focus stepper
-#define AP_RANGE 495						// # steps range for aperture stepper
+#define FOCUS_RANGE 2550      // # steps range for focus stepper
+#define AP_RANGE 495          // # steps range for aperture stepper
 
 
 typedef enum {full, roi, blob} ISCDisplayModeType;
 
 struct ISCStatusStruct {
   // General server state
-  int abort;			// 1 abort current execution thread
-  int pause;			// 1 is paused, 0 is continuous mode (doesn't apply to autofocus mode)
-  int save;				// 1 save frames
-  int autofocus;		// 1 auto focus on brightest blob in field
-  int focus_pos;		// stepper position for focus
-  int ap_pos;			// stepper position for aperture
-  int MCPFrameNum;		// current frame number of MCP
-  int shutdown;			// 0=nothing 1=shutdown 2=reboot computer 3=camera power cycle
-  int hold_current;	    // the hold "heater" current (0-50)
-  int exposure;			// exposure time in us REGARDLESS of self/hardware trigger
-  int triggertype;		// 0=software, 1=edge, 2=positive pulse, 3=negative pulse
-  int focusOffset;      // when camera focus ordered "home", step this far
-  int eyeOn;            // if set update the window
+  int abort;        // 1 abort current execution thread
+  int pause;        // 1 paused, 0 continuous (not autofocus mode)
+  int save;         // 1 save frames
+  int autofocus;    // 1 auto focus on brightest blob in field
+  int focus_pos;    // stepper position for focus
+  int ap_pos;       // stepper position for aperture
+  int MCPFrameNum;  // current frame number of MCP
+  int shutdown;     // 0=null 1=shutdown 2=reboot pc 3=power cycle cam
+  int hold_current; // the hold "heater" current (0-50)
+  int exposure;     // exposure time in us REGARDLESS of trigger
+  int triggertype;  // 0=software, 1=edge, 2=+ pulse, 3=- pulse
+  int focusOffset;  // when camera focus ordered "home" step this far
+  int eyeOn;        // if set update the window
 
   // Display mode parameters
   ISCDisplayModeType display_mode;
-  int roi_x;			// x pixel number for ROI
-  int roi_y;			// y pixel number for ROI
-  int blob_num;			// blob # for ROI
-  double azBDA;			// az tangent plane offset from BDA centre for the CCD (radians)
-  double elBDA;			// el      "      "     "    "    "    "    "  "
+  int roi_x;        // x pixel number for ROI
+  int roi_y;        // y pixel number for ROI
+  int blob_num;     // blob # for ROI
+  double azBDA;     // az tanplane offset BDA centre for the CCD (radians)
+  double elBDA;     // el      "      "     "    "    "    "    "  "
 
   // telescope attitude
-  double az;			// az in radians
-  double el;			// el in radians
-  double lst;			// lst in radians (!!)
-  double lat;			// North Lat in radians
+  double az;     // az in radians
+  double el;     // el in radians
+  double lst;    // lst in radians (!!)
+  double lat;    // North Lat in radians
 
   // "brightest star is..." state
-  int brightStarMode;	// 1 brightest star in field is at:
-  double brightRA;		// RA: radians (apparent)
-  double brightDEC;		// DEC: radians (apparent)
+  int brightStarMode; // 1 brightest star in field is at:
+  double brightRA;    // RA: radians (apparent)
+  double brightDEC;   // DEC: radians (apparent)
 
   // blob algorithm stuff
   double sn_threshold;
-  int grid;	// ask ed
-  int mult_dist;	// ed knows
+  int grid;        // ask ed
+  int mult_dist;   // ed knows
 
   // gain/offset
-  double gain;			// relative gain to the CCD factory default
-  int offset;			// offset to the CCD factory default (in digitized units)
+  double gain;    // relative gain to the CCD factory default
+  int offset;     // offset to CCD factory default (in digitized units)
 
   // Matching algorithm
-  int minBlobMatch;		// minimum # blobs used in the frame matching
-  int maxBlobMatch;		// maximum # blobs used in the frame matching
-  double mag_limit;		// size of tires
-  double norm_radius;	// search radius in normal mode
-  double lost_radius;	// search radius when lost
-  double tolerance;		// star asociation tolerance
-  double match_tol;		// % of blobs required for a match
-  double quit_tol;		// match_tol sufficient to exit from algorithm without testing all combinations
-  double rot_tol;		// rotation tolerance for blob finding
+  int minBlobMatch;    // minimum # blobs used in the frame matching
+  int maxBlobMatch;    // maximum # blobs used in the frame matching
+  double mag_limit;    // size of tires
+  double norm_radius;  // search radius in normal mode
+  double lost_radius;  // search radius when lost
+  double tolerance;    // star asociation tolerance
+  double match_tol;    // % of blobs required for a match
+  double quit_tol;     // match_tol sufficient to exit from algorithm w/o
+                       // testing all combinations
+  double rot_tol;      // rotation tolerance for blob finding
 };
 
 struct ISCSolutionStruct {
-  int flag;             // 0=ready for pulse, 1=pointing solution
-  int triggertype;      // 0=software, 1=edge, 2=positive pulse, 3=negative pulse
-  int cameraerr;		// 0 if everything is Kosher, otherwise problem...
-  int framenum;			// ISC frame # (also file names for images)
-  int MCPFrameNum;		// last MCP frame number received before image processing began
-  double mapMean;		// mean value of the map
-  double ra;			// RA of CCD in radians (apparent)
-  double dec;			// DEC   "        "        "
-  double sigma;			// uncertainty of solution in radians (or 2PI on failure)
-  double rot;			// rotation of the CCD field in radians (parallactic angle + constant)
+  int flag;        // 0=ready for pulse, 1=pointing solution
+  int triggertype; // 0=software, 1=edge, 2=+ pulse, 3=- pulse
+  int cameraerr;   // 0 if everything is Kosher, otherwise problem...
+  int framenum;    // ISC frame # (also file names for images)
+  int MCPFrameNum; // last MCP frame number received b/f image processing
+  double mapMean;  // mean value of the map
+  double ra;       // RA of CCD in radians (apparent)
+  double dec;      // DEC   "        "        "
+  double sigma;    // uncertainty of sol. in radians (or 2PI on failure)
+  double rot;      // rot. of CCD field (rad) = par. angle + constant
 
-  double temp1;	        // temperature sensors
+  double temp1;         // temperature sensors
   double temp2;
   double temp3;
   double temp4;
@@ -128,7 +134,7 @@ struct ISCSolutionStruct {
 };
 
 #ifdef _WINDOWS_
-#pragma pack()		// go back to default packing
+#pragma pack()    // go back to default packing
 #endif
 
 #endif
