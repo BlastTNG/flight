e30f184072cd26c373864d474d7f45dc460fdb70
Don Wiebe <dvw@ketiltrout.net>
2006-07-18 22:09:23 +0000

    Update encoder offset

--- mcp/share/channels.h
+++ mcp/share/channels.h
@@ -42,7 +42,7 @@ extern "C" {
 
   /* offset of encoder.  Reset if encoder has been unmounted. */
   /* This is the elevation at which the encoder wraps around */
-#define ENC_ELEV_OFFSET 233.90
+#define ENC_ELEV_OFFSET 234.476
   /* to get proper wrapping in KST, the encoder elevation type should be
    * 'u' for 135 <= ENC_ELEV_OFFSET < 315 and 's' otherwise */
 #define ENC_ELEV_TYPE 'u'
