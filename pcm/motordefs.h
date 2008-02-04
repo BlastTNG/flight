b2676266cd40646ef4d0408de2e9b356fecb9c7c
Anonymous User <none@none.com>
2008-02-04 18:52:26 +0000

    Adding in more motor files... lmf

---
+++ pcm/motordefs.h
@@ -0,0 +1,14 @@
+/*
+ * motordefs.h: Contains any functions or structures used by both
+ *              pivotcommand.c and reactcommand.c
+ *
+ */
+
+#ifndef MOTORDEFS_H
+#define MOTORDEFS_H
+
+#include <stdarg.h>  /* ANSI C variable arguments (va_list, va_start, va_end) */
+
+enum CheckType {resp, comm, both};
+
+#endif
