5f7e170f8b55e046b780dee70bee9f988b1756b1
Laura Fissel <fissel@astro.utoronto.ca>
2009-08-26 23:11:21 +0000

    Created copleycommand.c and copleycommand.h to store Copley accelnet controller specific commands for the flywheel and elevation controllers.  Added both to the Makefile.

---
+++ mcp/copleycommand.c
@@ -0,0 +1,16 @@
+#include <sys/select.h>
+#include <stdio.h>   /* Standard input/output definitions */
+#include <string.h>  /* String function definitions */
+#include <unistd.h>  /* UNIX standard function definitions */
+#include <fcntl.h>   /* File control definitions */
+#include <errno.h>   /* Error number definitions */
+#include <termios.h> /* POSIX terminal control definitions */
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <math.h>
+
+#include "blast.h"
+#include "copleycommand.h"
---
+++ mcp/copleycommand.h
@@ -0,0 +1,13 @@
+/*
+ * copleycommand.h: Header file for commands to be sent to 
+ * the Copley Accelnet EtherCAT motor controllers (flywheel 
+ * and elevation drive) on BLAST.
+ *
+ */
+
+#ifndef COPLEYCOMMAND_H
+#define COPLEYCOMMAND_H
+
+#include <stdarg.h>  /* ANSI C variable arguments (va_list, va_start, va_end) */
+
+#endif
