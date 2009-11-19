6ecfc187fe9d730da512f56356c8d6c7cd13af18
Steve Benton <sbenton@physics.utoronto.ca>
2009-11-19 02:19:19 +0000

    fix osc_maxage added to isc's command group

--- blastcmd/command_list_blast.c
+++ blastcmd/command_list_blast.c
@@ -22,7 +22,7 @@
 #include "command_list.h"
 #include "isc_protocol.h"  /* required for constants */
 
-const char *command_list_serial = "$Revision: 4.13 $";
+const char *command_list_serial = "$Revision: 4.14 $";
 
 const char *GroupNames[N_GROUPS] = {
   "Pointing Modes",        "Balance",          "Cool (empty)",
@@ -951,7 +951,7 @@ struct mcom mcommands[N_MCOMMANDS] = {
       {"Offset", -4096, 4096, 'i', "OSC_CCD_OFFSET"}
     }
   },
-  {COMMAND(osc_maxage), "set maximum delay between trigger and solution (ms)", GR_ISC_PARAM, 1,
+  {COMMAND(osc_maxage), "set maximum delay between trigger and solution (ms)", GR_OSC_PARAM, 1,
     {
       {"Max Age", 0, 10000, 'i', "OSC_MAX_AGE"},
     }
