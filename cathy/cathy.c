3b3314292580bc9b698153e630673537760eb875
Matthew Truch <matthew@truch.net>
2010-02-03 18:37:47 +0000

    Make a hello-world version compile

--- cathy/cathy.c
+++ cathy/cathy.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <getdata.h>
 
-int main (void)
+#define DIRFILE_DEFAULT "/data/etc/defile.lnk"
+#define CHATTER "chatter"
+
+int main (int argc, char **argv)
 {
+  char error_buffer[1024];
+  DIRFILE *dirfile;
+
+  if (argc >= 2)
+    dirfile_open(argv[1], GD_RDONLY);
+  else
+    dirfile_open(DIRFILE_DEFAULT, GD_RDONLY);
+
+  if (get_error(dirfile))
+  {
+    fprintf(stderr, "GetData error: %s\n", get_error_string(dirfile, error_buffer, 1024));
+    dirfile_close(dirfile);
+    exit(-1);
+  }
+
+  
   return 0;
 }
