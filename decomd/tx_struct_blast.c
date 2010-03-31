1f6c9db146735dfaf0b9e29a968722357664d538
Juan Soler <solerpu@iap.u-psud.fr>
2010-03-31 22:53:15 +0000

    veto_bal rescaling

--- decomd/tx_struct_blast.c
+++ decomd/tx_struct_blast.c
@@ -354,7 +354,7 @@ struct ChannelStruct SlowChannels[] = {
   {"level_off_bal",'w', LOOP1, 30,           1./1990.13,             0.0, 'u', U_I_A},
   {"level_target_bal",'w', LOOP1, 31,           1./1990.13,            -5.0, 'u', U_I_A},
   /* LOOP1 32-33 are wide */
-  {"veto_bal",     'w', LOOP1, 34,                4.0,             0.0, 's', U_NONE},
+  {"veto_bal",     'w', LOOP1, 34,           4.0 / SR,             0.0, 's', U_NONE},
   /* LOOP1 35-36 are unused */
   {"sip_alt",      'w', LOOP1, 37,                1.0,             0.0, 'u', U_NONE},
   /* LOOP1 38-41 are wide */
--- mcp/auxiliary.c
+++ mcp/auxiliary.c
@@ -249,15 +249,13 @@ static int Balance(int bits_bal)
     vPumpBalAddr = GetNiosAddr("v_pump_bal");
     modeBalAddr = GetNiosAddr("mode_bal");
   }
-
   /*
   if(k%100==0){
-  bprintf(info,"BALANCE: veto_val=%d",CommandData.pumps.veto_bal);
-  bprintf(info,"BALANCE: nw      =%d",CommandData.pointing_mode.nw);
+    bprintf(info,"BALANCE: veto_val=%d",CommandData.pumps.veto_bal);
+    bprintf(info,"BALANCE: nw      =%d",CommandData.pointing_mode.nw);
   }
   k++;
-  */
-  
+  */ 
   // enable slew mode
   if (CommandData.pumps.veto_bal == 0){
      CommandData.pumps.veto_bal = CommandData.pointing_mode.nw;
