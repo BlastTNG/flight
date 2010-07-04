8a95c3306b2c142c205a0b65ed52d564242a5ec5
Natalie Gandilo <ngandilo@gmail.com>
2010-07-04 15:44:15 +0000

    fixed to not include sbsc_protocol

--- narsil/src/command_list.c
+++ narsil/src/command_list.c
@@ -21,9 +21,11 @@
 
 #include "command_list.h"
 #include "isc_protocol.h"  /* required for constants */
+#ifdef __MCP__
 #include "sbsc_protocol.h"
+#endif
 
-const char *command_list_serial = "$Revision: 4.66 $";
+const char *command_list_serial = "$Revision: 4.67 $";
 
 const char *GroupNames[N_GROUPS] = {
   "Pointing Modes",        "Balance",          "Waveplate Rotator",
