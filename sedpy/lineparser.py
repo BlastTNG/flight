4c0e2835d142fde416340ab32840dcf72eef106e
Steve Benton <sbenton@physics.utoronto.ca>
2011-05-16 12:41:21 +0000

    allow global references for jack and cable numbers, to prevent changes small updates to tests to check new features fixed bug: p2p cable descriptions swallowed the length as well regular expressions reformatted, add matched to start and end of line

--- sedpy/lineparser.py
+++ sedpy/lineparser.py
@@ -3,28 +3,56 @@
 import re
 from sedobjects import *
 
-#LABEL -> MATE pins "description" alpha bcd M-Mmate F-Fmate
-connRE = re.compile(r'([A-Z0-9_]{1,16})\s+->\s+([A-Z0-9_]{1,16})\s+(\d{1,3})'\
-    r'\s+"([^"]{1,255})"\s+([yYnN])\s+([yYnN])\s+M-([mMfFxX])\s+F-([mMfFxX])')
+#LABEL -> MATE pins "description" [alpha] [bcd] [M-Mmate] [F-Fmate]
+connRE = re.compile(r'^'
+    r'([A-Z0-9_]{1,16})\s+->\s+'  #LABEL
+    r'([A-Z0-9_]{1,16})\s+'	  #MATE
+    r'(\d{1,3})\s+'		  #pins
+    r'"([^"]{1,255})"'		  #description
+    r'(?:\s*(alpha))?'		  #alpha
+    r'(?:\s*(bcd))?'		  #bcd
+    r'(?:\s+M-([mMfF]))?'	  #mmate
+    r'(?:\s+F-([mMfF]))?'	  #fmate
+    r'$')
 
-#NAME_1 "Long form of name" [< PARENT]
-compRE = re.compile(r'([A-Z0-9_]{1,8})\s+"([^"]{1,65})"' \
-    r'(?:\s+<\s+([A-Z0-9_]{1,8}))?')
+#CMPNAME "Description of the COMPONENT" [< PARTOF]
+compRE = re.compile(r'^'
+    r'([A-Z_][A-Z0-9_]{0,7})\s+'    #CMPNAME
+    r'"([^"]{1,65})"'		    #description
+    r'(?:\s+<\s+([A-Z0-9_]{1,8}))?' #PARTOF
+    r'$')
 
-#JACK [IN] ref "label" conn/g -> dest_part[/jackref] [CABLE "desc" [len]]
-jackRE = re.compile(r'JACK(?:\s*(IN))?\s*([a-z0-9]+)\s+"([^"]{1,32})"\s+'\
-    r'([A-Z0-9_]{1,16}/[mMfF])\s+->\s+([A-Z0-9_]{1,8})(?:/([a-z0-9]+))?'\
-    r'(?:\s+CABLE\s+"([^"]{0,64})(?:\s+(\d+))?)?')
+#JACK [IN] [&]ref "label" CONN/G -> DEST[/jack] [CABLE [&C##] "[desc]" [len]]
+jackRE = re.compile(r'^JACK'
+    '(?:\s*(IN))?\s+'			#IN
+    r'((?:&J?\d{1,5})|[a-z0-9]+)\s+'	#ref
+    r'"([^"]{1,32})"\s+'		#label
+    r'([A-Z0-9_]{1,16}/[mMfF])\s+->\s+'	#CONN/G
+    r'(&?[A-Z0-9_]{1,8})'		#DEST
+    r'(?:/((?:&J?\d{1,5})|[a-z0-9]+))?'	#/jack
+    r'(?:\s+CABLE'			#CABLE
+      r'(?:\s+(&C?[0-9]{1,8}))?\s+'	#   &C##
+      r'"([^"]{0,64})"'			#   desc
+      r'(?:\s+(\d+))?'			#   len
+    r')?$')
 
-#LINE "description" (jack#,pin#[,pin#,...])[,(jack#,pin#s),...]
-lineRE = re.compile(r'LINE\s+"([^"]{1,64})"\s+'\
-    r'((?:\([a-z0-9]+[;,](?:\s*[A-Za-z0-9]{1,3},?)*\),?\s*)+)')
+#LINE "description" (jack;pin[,pin2,...])[,(jack;pin[,pin2,...]), ...]
+lineRE = re.compile(r'^LINE\s+'
+    r'"([^"]{1,64})"\s+'		    #description
+    r'((?:'
+    r'\((?:(?:&J?\d{1,5})|[a-z0-9]+)[;,]'   #jack
+    r'(?:\s*[A-Za-z0-9]{1,3},?)*\),?\s*'    #pin
+    r')+)$')
 
-#CABLE key "description" [len]
-cableRE = re.compile(r'CABLE\s+(C?[0-9]{1,8})\s+"([^"]{1,64})"(?:\s+([0-9]+))?')
+#CABLE [&]C## "description" [len]
+cableRE = re.compile(r'^CABLE\s+'
+    r'(&?C?[0-9]{1,8})\s+'  #C##
+    r'"([^"]{1,64})"'	    #description
+    r'(?:\s+([0-9]+))?'	    #len
+    r'$')
 
 #lookup table of regular expressions corresponding to object classes
-RElookup = {Connector: connRE, Component: compRE, Jack: jackRE, \
+RElookup = {Connector: connRE, Component: compRE, Jack: jackRE, 
     Line: lineRE, Cable: cableRE}
 
 def parse(type, line):
@@ -37,51 +65,55 @@ def parse(type, line):
 
 
 if __name__ == "__main__":
-  print "COMPONENT tests"
+  print "\nCONNECTOR tests"
+  conn1 = ('TYPE -> MATE 10 "TYPE has 10 male pins that go with female MATE" '
+      'alpha bcd M-F')
+  conn2 = 'CONN -> CONN 0 "A connector with variable num of pins" alpha F-F'
+  conn3 = 'COMM -> COMM 0 "A connector with non-alphanumeric pins" F-F'
+  conn4 = 'TEST -> TEST 10 "A connector to test jack mating" M-F F-M'
+  print (conn1), '\n', parse(Connector,conn1)
+  print (conn2), '\n', parse(Connector, conn2)
+  print (conn3), '\n', parse(Connector, conn3)
+  test_connector = parse(Connector, conn4)
+  test_connector.mate = test_connector
+  print (conn4), '\n', test_connector
+
+  print "\nCOMPONENT tests"
   comp1 = 'COMPA "COMPA is part of COMP1" < COMP1'
   comp2 = 'COMP1 "COMP1 is not part of anything"'
-  comp3 = 'some random string that does not match'
-  print repr(comp1), '\ngives:', parse(Component,comp1)
-  print repr(comp2), '\ngives:', parse(Component,comp2)
-  print repr(comp3), '\ngives:', parse(Component,comp3)
-
-  print "\nCONNECTOR tests"
-  conn1 = 'TYPE -> MATE 10 "TYPE has 10 male pins that go with female MATE"\
-      y y M-F F-X'
-  conn2 = 'CONN -> CONN 0 "A connector with variable num of pins" y y M-M F-X'
-  print repr(conn1), '\ngives', parse(Connector,conn1)
-  connector = parse(Connector, conn2)
-  connector.mate = connector
-  print repr(conn2), '\ngives', connector
+  comp3 = 'some random string that does not match'    #bad declaration
+  print (comp1), '\n', parse(Component,comp1)
+  print (comp2), '\n', parse(Component,comp2)
+  print (comp3), '\n', parse(Component,comp3)
 
   print "\nCABLE tests"
   cab1 = 'CABLE 12345 "This cable has no length"'
-  cab2 = 'CABLE 12346 "This cable is 350mm long" 350'
-  print repr(cab1), '\ngives', parse(Cable, cab1)
-  print repr(cab2), '\ngives', parse(Cable, cab2)
+  cab2 = 'CABLE &C12346 "This cable is 350mm long" 350'
+  print (cab1), '\n', parse(Cable, cab1)
+  print (cab2), '\n', parse(Cable, cab2)
 
   print "\n JACK tests"
-  jack1 = 'JACK1 "connects to COMPA with no cable" CONN/M -> COMPA'
-  jack2 = 'JACK2 "now has cable" CONN2/f -> COMPB/j12 CABLE "cable, len=?"'
-  jack3 = 'JACK j3 "now has cable" CONN2/f -> COMPB CABLE "cable, len=300" 300'
-  jack4 = 'JACK 4 "cable desc is blank" CONN/m -> COMP CABLE ""'
-  jack5 = 'JACK 5 "a possible mate for jack 4" CONN/M -> COMP CABLE "soemthing"'
-  print repr(jack1), '\ngives', parse(Jack, jack1)
-  print repr(jack2), '\ngives', parse(Jack, jack2)
-  print repr(jack3), '\ngives', parse(Jack, jack3)
+  jack1 = 'JACK 1 "connects to COMPA with no cable" CONN/M -> COMPA'
+  jack2 = 'JACK &J2 "now has cable" CONN2/F -> COMPB/2 CABLE &C4 "cable, len=?"'
+  jack3 = 'JACK j3 "now has cable" CONN2/F -> COMPB CABLE "cable, len=300" 300'
+  jack4 = 'JACK 4 "cable desc is blank" TEST/M -> COMP CABLE ""'
+  jack5 = 'JACK 5 "a possible mate for jack 4" TEST/M -> COMP CABLE "soemthing"'
+  print (jack1), '\n', parse(Jack, jack1)
+  print (jack2), '\n', parse(Jack, jack2)
+  print (jack3), '\n', parse(Jack, jack3)
   j4 = parse(Jack, jack4)
-  j4.conn = connector
-  print repr(jack4), '\ngives', j4
+  j4.conn = test_connector
+  print (jack4), '\n', j4
   j5 = parse(Jack, jack5)
-  j5.conn = connector
-  print repr(jack5), '\ngives', j5
-  if j5.canMate(j4): print "It can mate with the previous jack"
-  else: print "It can't mate with the previous jack"
+  j5.conn = test_connector
+  print (jack5), '\n', j5
+  if j5.canMate(j4): print "It CAN mate with jack 4"
+  else: print "It CAN NOT mate with jack 4"
 
   print "\nLINE tests"
-  line1 = 'LINE "this line has one pin" (0,1)'
+  line1 = 'LINE "this line has one pin" (&J22,1)'
   line2 = 'LINE "this one has 4 pins" (0,1),(0,2),(1,A),(1,C)'
   line3 = 'LINE "shortform for above" (0;1,2),(1;A,C)'
-  print repr(line1), '\ngives', parse(Line, line1)
-  print repr(line2), '\ngives', parse(Line, line2)
-  print repr(line3), '\ngives', parse(Line, line3)
+  print (line1), '\n', parse(Line, line1)
+  print (line2), '\n', parse(Line, line2)
+  print (line3), '\n', parse(Line, line3)
