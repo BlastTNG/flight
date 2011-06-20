87128095081b57dd025bc44f441f0f1958e7e431
Steve Benton <sbenton@physics.utoronto.ca>
2011-06-20 17:49:34 +0000

    I don't think I need "LOCAL", but that makes it work...

--- sedpy/replacetables.sql
+++ sedpy/replacetables.sql
@@ -6,10 +6,10 @@ DELETE FROM jack;
 DELETE FROM line;
 DELETE FROM pin;
 
-LOAD DATA INFILE '/data/sedweb/out/cable' INTO TABLE cable;
-LOAD DATA INFILE '/data/sedweb/out/component' INTO TABLE component;
-LOAD DATA INFILE '/data/sedweb/out/congender' INTO TABLE congender;
-LOAD DATA INFILE '/data/sedweb/out/connector' INTO TABLE connector;
-LOAD DATA INFILE '/data/sedweb/out/jack' INTO TABLE jack;
-LOAD DATA INFILE '/data/sedweb/out/line' INTO TABLE line;
-LOAD DATA INFILE '/data/sedweb/out/pin' INTO TABLE pin;
+LOAD DATA LOCAL INFILE '/data/sedweb/out/cable' INTO TABLE cable;
+LOAD DATA LOCAL INFILE '/data/sedweb/out/component' INTO TABLE component;
+LOAD DATA LOCAL INFILE '/data/sedweb/out/congender' INTO TABLE congender;
+LOAD DATA LOCAL INFILE '/data/sedweb/out/connector' INTO TABLE connector;
+LOAD DATA LOCAL INFILE '/data/sedweb/out/jack' INTO TABLE jack;
+LOAD DATA LOCAL INFILE '/data/sedweb/out/line' INTO TABLE line;
+LOAD DATA LOCAL INFILE '/data/sedweb/out/pin' INTO TABLE pin;
