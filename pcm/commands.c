b886619aee69f3b95e92e298a03b8758e3ff5fa0
Steve Benton <sbenton@physics.utoronto.ca>
2011-09-25 19:56:33 +0000

    pck some vaguesly sensible default bias settings

--- pcm/commands.c
+++ pcm/commands.c
@@ -2425,10 +2425,10 @@ void InitCommandData()
     CommandData.hk[i].fplo_heat = 0.0;
     CommandData.hk[i].ssa_heat = 0.0;
 
-    CommandData.hk[i].cernox.phase = 0.0;
-    CommandData.hk[i].cernox.ampl = 0.0;
-    CommandData.hk[i].ntd.phase = 0.0;
-    CommandData.hk[i].ntd.ampl = 0.0;
+    CommandData.hk[i].cernox.phase = 5.0;
+    CommandData.hk[i].cernox.ampl = 0.3;
+    CommandData.hk[i].ntd.phase = 5.0;
+    CommandData.hk[i].ntd.ampl = 0.5;
   }
   CommandData.hk_last = 0;
   CommandData.hk_bias_freq = 50;
