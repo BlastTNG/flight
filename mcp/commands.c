3807143e2a16f25d3ed0f7aead4a0dcbcda36e7d
Laura Fissel <fissel@astro.utoronto.ca>
2012-03-27 16:53:47 +0000

    Changed el encoder LOCK_OFFSET so mcp knows at which elevations it can lock.

--- mcp/commands.c
+++ mcp/commands.c
@@ -49,7 +49,7 @@
  * 90 degrees.  This is the offset to the true lock positions.
  * This number is relative to the elevation encoder reading, NOT
  * true elevation */
-#define LOCK_OFFSET (-1.4) /* Updated by LMF on December 9th, 2010 */
+#define LOCK_OFFSET (-0.25) /* Updated by LMF on December 9th, 2010 */
 
 /* based on isc_protocol.h */
 #define ISC_SHUTDOWN_NONE     0
