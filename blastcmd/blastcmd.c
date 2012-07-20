e86aaec2c60dffb52f66eb6292d14204cf606e84
Natalie Gandilo <ngandilo@gmail.com>
2012-07-20 17:00:22 +0000

    Fixed conflict.

--- blastcmd/blastcmd.c
+++ blastcmd/blastcmd.c
@@ -53,7 +53,6 @@ double round(double x);
 #define ACK_COUNT 17
 
 #define INPUT_TTY "/dev/ttyCMD"
-//#define INPUT_TTY "/dev/ttyUSB0"
 #define LOGFILE DATA_ETC_DIR "/blastcmd.log"
 
 int silent = 0;
