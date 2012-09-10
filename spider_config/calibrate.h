9a6778eeb89557634b31972aa56a5e51f7987953
Jamil Shariff <shariff@astro.utoronto.ca>
2012-09-10 19:00:40 +0000

    change encoder 1 offset

--- spider_config/calibrate.h
+++ spider_config/calibrate.h
@@ -163,7 +163,7 @@ extern "C" {
 #define MAGZ_M (-1/1290.0)
 #define MAGZ_B (32768.0)
 
-#define ENC1_OFFSET (-8.388134765625) // encoder 1 to bore-sight offset angle
+#define ENC1_OFFSET (-11.9531923828125) // encoder 1 to bore-sight offset angle
 #define ENC2_OFFSET 111.87919921875  // encoder 2 to bore-sight offset angle
 #define GYBOX_OFFSET -69.8  // gyro box to bore-sight offset angle (fixed)
 
