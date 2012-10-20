5b965791b184cce3d21538451c46284da9711be4
Natalie Gandilo <ngandilo@gmail.com>
2012-10-20 18:44:02 +0000

    fixed Star Camera Ethernet status fields

--- spider_config/derived.c
+++ spider_config/derived.c
@@ -106,8 +106,9 @@ union DerivedUnion DerivedChannels[] = {
       "BLAST_SUCKS"),
   BITWORD("SCHEDULE", "status_mcc", 4, 3),
   BITWORD("SLOT_SCHED", "status_mcc", 8, 8),
-  BITWORD("STATUS_RSC_ETH", "status_eth", 0, 2),
-  BITWORD("STATUS_BSC_ETH", "status_eth", 2, 2),
+  BITWORD("STATUS_GOOD_ETH", "status_eth", 0, 2),
+  BITWORD("STATUS_BAD_ETH", "status_eth", 2, 2),
+  BITWORD("STATUS_UGLY_ETH", "status_eth", 4, 2),
 
 #ifndef BOLOTEST
   COMMENT("Pointing Stuff"),
