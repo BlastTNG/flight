936e0c7365f55eb282a6cfa9c4748c89ebb24b11
Steve Benton <sbenton@physics.utoronto.ca>
2013-08-02 02:29:08 +0000

    update rowlen

--- pcm/set_sync.sh
+++ pcm/set_sync.sh
@@ -8,6 +8,6 @@ TTY=/dev/ttyS0
 
 stty -F $TTY ispeed 9600 ospeed 9600 cs8 -cstopb -parenb -crtscts
 # echo -en 'rl 41\r' > $TTY
-echo -en 'rl 55\r' > $TTY
+echo -en 'rl 50\r' > $TTY
 echo -en 'nr 33\r' > $TTY
 echo -en 'fr 120\r' > $TTY
