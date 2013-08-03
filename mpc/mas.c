5638094c257c137c7e05b446271cb7e92f11819f
Don Wiebe <dvw@ketiltrout.net>
2013-08-03 19:59:21 +0000

    Pick TES biases.

--- mpc/mas.c
+++ mpc/mas.c
@@ -907,6 +907,36 @@ static int do_ivcurve(uint32_t kickbias, int kickwait, int start, int last,
   return exec_and_wait(sched, none, argv[0], (char**)argv, 0, 1, &kill_special);
 }
 
+static void pick_biases(int iv_num)
+{
+  struct mcp_proc *p;
+  int p_stdout = 0;
+  char biases[1024];
+  int data[256];
+
+  char ivname[256];
+  sprintf(ivname, "iv_%04i", iv_num);
+  char *argv[] = { "/data/mas/bin/choose_tes_bias", ivname, NULL };
+
+  p = start_proc(argv[0], argv, 0, 1, NULL, &p_stdout, NULL,
+      &kill_special);
+  if (p == NULL)
+    return;
+  
+  /* capture the output */
+  read(p_stdout, &biases, 1024);
+
+  stop_proc(p, 0, 0, 0, 0);
+
+  if (sscanf(biases, "%i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i",
+        data + 0, data + 1, data + 2, data + 3, data + 4, data + 5, data + 6,
+        data + 7, data + 8, data + 9, data + 10, data + 11, data + 12,
+        data + 13, data + 14, data + 15) == 16)
+  {
+    write_param("tes", "bias", 0, data, 16);
+  }
+}
+
 /* run and archive an iv curve */
 static int ivcurve(void)
 {
@@ -940,6 +970,10 @@ static int ivcurve(void)
         bprintf(info, "Archived IV curve as %s/iv_%04i", basedir,
             memory.last_iv);
       }
+
+    /* analyse */
+    pick_biases(memory.last_iv);
+
   }
 
   return 0;
