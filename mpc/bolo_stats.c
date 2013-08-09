721a62465ff7a244f2146d959236498cabb9540e
Sasha <unknown@none.com>
2013-08-09 08:50:37 +0000

    bolo stat local variables

--- mpc/bolo_stats.c
+++ mpc/bolo_stats.c
@@ -23,11 +23,10 @@ int sb_top = 0;
 /* extract data from frame (assumes data mode 10!) */
 #define FRAME_EXTRACT(frm,idx) (double)(frm[idx + MCE_HEADER_SIZE] >> 7)
 
-static void set_filter_coeffs(const double fsamp)
+static void set_filter_coeffs(const double fsamp, const double flow, const double fup)
 {
+  
   /* warp frequencies */
-  double flow = memory.bolo_filt_freq * ( 1. - memory.bolo_filt_bw / 2. );
-  double fup = memory.bolo_filt_freq * ( 1. + memory.bolo_filt_bw / 2. );
   double oml = tan(M_PI * flow / fsamp);
   double omu = tan(M_PI * fup / fsamp);
   double om02 = oml*omu;
@@ -52,8 +51,8 @@ static void set_filter_coeffs(const double fsamp)
   filt_coeffa[3] = (-2. + rt2dom - rt2dom*om02 + 2.*om04)*2./norm;
   filt_coeffa[4] = (1. - rt2dom - rt2dom*om02 + 2.*om02 + dom2 + om04)/norm;
 
-  bprintf(info, "Science band: %.2f Hz center, %.1f\% bandwidth, at sample rate of %.2f",
-	  memory.bolo_filt_freq, memory.bolo_filt_bw*100, fsamp);
+  bprintf(info, "Science band: %.2f Hz center, %.2f Hz bandwidth, at sample rate of %.2f",
+	  (flow + fup) / 2., flow - fup, fsamp);
   bprintf(info, "Science band filter: b = [ %.4f, %.4f, %.4f, %.4f, %.4f ]",
 	  filt_coeffb[0], filt_coeffb[1], filt_coeffb[2], filt_coeffb[3], filt_coeffb[4]);
   bprintf(info, "Science band filter: a = [ %.4f, %.4f, %.4f, %.4f, %.4f ]",
@@ -63,6 +62,11 @@ static void set_filter_coeffs(const double fsamp)
 void update_stats(const uint32_t *curr_frame, size_t frame_size, uint32_t frameno)
 {
   const struct mas_header *header = (const struct mas_header *)curr_frame;
+  
+  static double loc_filt_freq, loc_filt_bw;
+  static int loc_filt_len;
+  static double loc_bs_gain[N_STAT_TYPES];
+  static int loc_bs_offset[N_STAT_TYPES];
   
   int ii, jj, sb_idx[NUM_FILT_COEFF], fb_idx[NUM_FILT_COEFF];
   size_t ndata = frame_size / sizeof(uint32_t) - MCE_HEADER_SIZE - 1;
@@ -75,18 +79,29 @@ void update_stats(const uint32_t *curr_frame, size_t frame_size, uint32_t framen
   if (ndata > NUM_COL * NUM_ROW) ndata = NUM_COL * NUM_ROW;
   
   if (stat_reset == 1) {
-    /* recalculate filter coefficients */
-    set_filter_coeffs((const double) fsamp);
 
-    bprintf(info, "Statistics: buffer %d", memory.bolo_filt_len);
+    loc_filt_len = memory.bolo_filt_len;
+    loc_filt_freq = memory.bolo_filt_freq;
+    loc_filt_bw = memory.bolo_filt_bw;
+    for (ii = 0; ii < N_STAT_TYPES; ii++) {
+      loc_bs_gain[ii] = memory.bolo_stat_gain[ii];
+      loc_bs_offset[ii] = memory.bolo_stat_offset[ii];
+    }
+    
+    /* recalculate filter coefficients */
+    set_filter_coeffs((const double) fsamp,
+		      (const double) loc_filt_freq * ( 1. - loc_filt_bw / 2. ),
+		      (const double) loc_filt_freq * ( 1. + loc_filt_bw / 2. ));
+    
+    bprintf(info, "Statistics: buffer %d", loc_filt_len);
     bprintf(info, "Statistics: gains M = %.2f, S = %.2f, N = %.2f",
-	    memory.bolo_stat_gain[bs_mean],
-	    memory.bolo_stat_gain[bs_sigma],
-	    memory.bolo_stat_gain[bs_noise]);
+	    loc_bs_gain[bs_mean],
+	    loc_bs_gain[bs_sigma],
+	    loc_bs_gain[bs_noise]);
     bprintf(info, "Statistics: offsets M = %i, S = %i, N = %i",
-	    memory.bolo_stat_offset[bs_mean],
-	    memory.bolo_stat_offset[bs_sigma],
-	    memory.bolo_stat_offset[bs_noise]);
+	    loc_bs_offset[bs_mean],
+	    loc_bs_offset[bs_sigma],
+	    loc_bs_offset[bs_noise]);
     
     /* reset buffers */
     sb_top = 0;
@@ -108,13 +123,13 @@ void update_stats(const uint32_t *curr_frame, size_t frame_size, uint32_t framen
   
   /* neighboring buffer elements */
   fb_idx[0] = (fb_top + 1) % FB_SIZE; // oldest element
-  sb_idx[0] = (sb_top + 1) % memory.bolo_filt_len; // oldest element
+  sb_idx[0] = (sb_top + 1) % loc_filt_len; // oldest element
   // previous elements for filtering
   for (jj = 1; jj < NUM_FILT_COEFF; jj++) {
     fb_idx[jj] = (fb_top - jj) % FB_SIZE;
     if (fb_idx[jj] < 0) fb_idx[jj] += FB_SIZE;
-    sb_idx[jj] = (sb_top - jj) % memory.bolo_filt_len;
-    if (sb_idx[jj] < 0) sb_idx[jj] += memory.bolo_filt_len;
+    sb_idx[jj] = (sb_top - jj) % loc_filt_len;
+    if (sb_idx[jj] < 0) sb_idx[jj] += loc_filt_len;
   }
   
   /* add new values */
@@ -135,23 +150,23 @@ void update_stats(const uint32_t *curr_frame, size_t frame_size, uint32_t framen
     frame_filt[sb_top][ii] = fdatum;
     
     /* update statistics */    
-    dmean = frame_sum[ii] / (double)(memory.bolo_filt_len);
-    dsigma = sqrt((frame_sum2[ii] - dmean * dmean * (double) memory.bolo_filt_len )
-		  / (double)(memory.bolo_filt_len - 1));
+    dmean = frame_sum[ii] / (double)(loc_filt_len);
+    dsigma = sqrt((frame_sum2[ii] - dmean * dmean * (double) loc_filt_len )
+		  / (double)(loc_filt_len - 1));
     
     sgn = (dmean > 0) - (dmean < 0);
-    if ( fabs(dmean) < memory.bolo_stat_offset[bs_mean] ) dmean = memory.bolo_stat_offset[bs_mean];
-    dmean = sgn * memory.bolo_stat_gain[0] * log (1. + fabs(dmean) - memory.bolo_stat_offset[0]);
+    if ( fabs(dmean) < loc_bs_offset[bs_mean] ) dmean = loc_bs_offset[bs_mean];
+    dmean = sgn * loc_bs_gain[0] * log (1. + fabs(dmean) - loc_bs_offset[0]);
     bolo_stat_buff[bs_mean][ii] = dmean + 128;
     
-    if ( dsigma < memory.bolo_stat_offset[bs_sigma] ) dsigma = memory.bolo_stat_offset[bs_sigma];
-    dsigma = memory.bolo_stat_gain[bs_sigma] * log ( 1. + dsigma - memory.bolo_stat_offset[bs_sigma] );
+    if ( dsigma < loc_bs_offset[bs_sigma] ) dsigma = loc_bs_offset[bs_sigma];
+    dsigma = loc_bs_gain[bs_sigma] * log ( 1. + dsigma - loc_bs_offset[bs_sigma] );
     bolo_stat_buff[bs_sigma][ii] = dsigma;
 
-    dnoise = sqrt(frame_fsum2[ii] / (double)(memory.bolo_filt_len - 1) /
-		  (double)(memory.bolo_filt_freq * memory.bolo_filt_bw));
-    if ( dnoise < memory.bolo_stat_offset[bs_noise] ) dnoise = memory.bolo_stat_offset[bs_noise];
-    dnoise = memory.bolo_stat_gain[bs_noise] * log ( 1. + dnoise - memory.bolo_stat_offset[bs_noise] );
+    dnoise = sqrt(frame_fsum2[ii] / (double)(loc_filt_len - 1) /
+		  (double)(loc_filt_freq * loc_filt_bw));
+    if ( dnoise < loc_bs_offset[bs_noise] ) dnoise = loc_bs_offset[bs_noise];
+    dnoise = loc_bs_gain[bs_noise] * log ( 1. + dnoise - loc_bs_offset[bs_noise] );
     bolo_stat_buff[bs_noise][ii] = dnoise;
     
     /* subtract oldest values from buffers */
@@ -162,5 +177,5 @@ void update_stats(const uint32_t *curr_frame, size_t frame_size, uint32_t framen
     frame_fsum2[ii] -= fdatum * fdatum;
   }
   
-  sb_top = (sb_top + 1) % memory.bolo_filt_len;
+  sb_top = (sb_top + 1) % loc_filt_len;
 }
