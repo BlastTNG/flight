89938a9c42b2de0ce32edbbee2f06a1278c2526a
Seth Hillbrand <seth.hillbrand@csus.edu>
2015-03-31 12:02:16 -0700

    Adding addressing, PDO data request code to ec_motors

--- mcp/include/ec_motors.h
+++ mcp/include/ec_motors.h
@@ -32,13 +32,13 @@
  * N.B. Here, RX/TX are from the controller's perspective, so RX is
  * received by the controller and TX is transmitted by the controller
  */
-#define EC_RXPDO_ASSIGNMENT 0x1c12
-#define EC_TXPDO_ASSIGNMENT 0x1c13
+#define ECAT_RXPDO_ASSIGNMENT 0x1c12
+#define ECAT_TXPDO_ASSIGNMENT 0x1c13
 
-#define EC_TXPDO_MAPPING 0x1a00
-#define EC_RXPDO_MAPPING 0x1600
+#define ECAT_TXPDO_MAPPING 0x1a00
+#define ECAT_RXPDO_MAPPING 0x1600
 
-#define EC_DC_CYCLE_NS 500000 /* Distributed Clock Cycle in nanoseconds */
+#define ECAT_DC_CYCLE_NS 500000 /* Distributed Clock Cycle in nanoseconds */
 
 typedef union {
     uint32_t val;
@@ -50,12 +50,116 @@ typedef union {
 } pdo_mapping_t;
 
 typedef enum {
-    EC_MOTOR_COLD,
-    EC_MOTOR_INIT,
-    EC_MOTOR_FOUND_PARTIAL,
-    EC_MOTOR_FOUND,
-    EC_MOTOR_RUNNING_PARTIAL,
-    EC_MOTOR_RUNNING
+    ECAT_MOTOR_COLD,
+    ECAT_MOTOR_INIT,
+    ECAT_MOTOR_FOUND_PARTIAL,
+    ECAT_MOTOR_FOUND,
+    ECAT_MOTOR_RUNNING_PARTIAL,
+    ECAT_MOTOR_RUNNING
 } ec_motor_state_t;
 
+
+#define COPLEY_ETHERCAT_VENDOR 0x000000ab
+#define AEP_090_036_PRODCODE 0x00000380
+#define BEL_090_030_PRODCODE 0x00001110
+
+/*******************************************************************
+ * This section encodes a number of CanBus/EtherCAT index/subindex values
+ * for the Copley motor controller commands.  This are referenced from
+ * http://www.copleycontrols.com/Motion/pdf/Parameter_Dictionary.pdf
+ *
+ * They are stored as index, subindex macros for use in registering
+ * PDO/SDO calls.
+ *
+ */
+
+#define ECAT_CURRENT_LOOP_CP 0x2380, 1 /* Proportional Gain UINT16 */
+#define ECAT_CURRENT_LOOP_CI 0x2380, 2 /* Integral Gain UINT16 */
+#define ECAT_CURRENT_LOOP_VAL 0x2340, 0 /* Actual current in 0.01A INT16 */
+#define ECAT_DRIVE_STATE 0x2300, 0 /* Desired state of the drive UINT16 */
+#  define ECAT_DRIVE_STATE_DISABLED 0
+#  define ECAT_DRIVE_STATE_PROG_CURRENT 1
+#  define ECAT_DRIVE_STATE_PROG_VELOCITY 11
+
+#define ECAT_MOTOR_POSITION 0x2240, 0 /* Encoder position in counts INT32 */
+#define ECAT_DRIVE_TEMP 0x2202, 0 /* A/D Reading in degrees C INT16 */
+
+#define ECAT_DRIVE_STATUS 0x1002, 0 /* Drive status bitmap UINT32 */
+#  define ECAT_STATUS_SHORTCIRCUIT          (1<<0)
+#  define ECAT_STATUS_DRIVE_OVERTEMP        (1<<1)
+#  define ECAT_STATUS_OVERVOLTAGE           (1<<2)
+#  define ECAT_STATUS_UNDERVOLTAGE          (1<<3)
+#  define ECAT_STATUS_TEMP_SENS_ACTIVE      (1<<4)
+#  define ECAT_STATUS_ENCODER_FEEDBACK_ERR  (1<<5)
+#  define ECAT_STATUS_PHASING_ERROR         (1<<6)
+#  define ECAT_STATUS_CURRENT_LIMITED       (1<<7)
+#  define ECAT_STATUS_VOLTAGE_LIMITED       (1<<8)
+#  define ECAT_STATUS_POS_LIMIT_SW          (1<<9)
+#  define ECAT_STATUS_NEG_LIMIT_SW          (1<<10)
+#  define ECAT_STATUS_ENABLE_NOT_ACTIVE     (1<<11)
+#  define ECAT_STATUS_SW_DISABLE            (1<<12)
+#  define ECAT_STATUS_STOPPING              (1<<13)
+#  define ECAT_STATUS_BRAKE_ON              (1<<14)
+#  define ECAT_STATUS_PWM_DISABLED          (1<<15)
+#  define ECAT_STATUS_POS_SW_LIMIT          (1<<16)
+#  define ECAT_STATUS_NEG_SW_LIMIT          (1<<17)
+#  define ECAT_STATUS_TRACKING_ERROR        (1<<18)
+#  define ECAT_STATUS_TRACKING_WARNING      (1<<19)
+#  define ECAT_STATUS_DRIVE_RESET           (1<<20)
+#  define ECAT_STATUS_POS_WRAPPED           (1<<21)
+#  define ECAT_STATUS_DRIVE_FAULT           (1<<22)
+#  define ECAT_STATUS_VEL_LIMIT             (1<<23)
+#  define ECAT_STATUS_ACCEL_LIMIT           (1<<24)
+#  define ECAT_STATUS_TRACK_WINDOW          (1<<25)
+#  define ECAT_STATUS_HOME_SWITCH_ACTIVE    (1<<26)
+#  define ECAT_STATUS_IN_MOTION             (1<<27)
+#  define ECAT_STATUS_VEL_WINDOW            (1<<28)
+#  define ECAT_STATUS_PHASE_UNINIT          (1<<29)
+#  define ECAT_STATUS_CMD_FAULT             (1<<30)
+
+#define ECAT_LATCHED_DRIVE_FAULT 0x2183, 0 /* Drive faults bitmap UINT32 */
+#  define ECAT_FAULT_DATA_CRC               (1<<0)
+#  define ECAT_FAULT_INT_ERR                (1<<1)
+#  define ECAT_FAULT_SHORT_CIRCUIT          (1<<2)
+#  define ECAT_FAULT_DRIVE_OVER_TEMP        (1<<3)
+#  define ECAT_FAULT_MOTOR_OVER_TEMP        (1<<4)
+#  define ECAT_FAULT_OVER_VOLT              (1<<5)
+#  define ECAT_FAULT_UNDER_VOLT             (1<<6)
+#  define ECAT_FAULT_FEEDBACK_FAULT         (1<<7)
+#  define ECAT_FAULT_PHASING_ERR            (1<<8)
+#  define ECAT_FAULT_TRACKING_ERR           (1<<9)
+#  define ECAT_FAULT_CURRENT_LIMIT          (1<<10)
+#  define ECAT_FAULT_FPGA_ERR1              (1<<11)
+#  define ECAT_FAULT_CMD_LOST               (1<<12)
+#  define ECAT_FAULT_FPGA_ERR2              (1<<13)
+#  define ECAT_FAULT_SAFETY_CIRCUIT_FAULT   (1<<14)
+#  define ECAT_FAULT_CANT_CONTROL_CURRENT   (1<<15)
+#  define ECAT_FAULT_WIRING_DISCONNECT      (1<<16)
+
+#define ECAT_CURRENT_LOOP_OFFSET 0x60F6, 3 /* Added to commanded current to compensate for drift. 0.01A INT16 */
+#define ECAT_MOTOR_ENC_WRAP_POS 0x2220, 0 /* Position value at which the motor encoder position will wrap. INT32 */
+
+#define ECAT_CTL_STATUS 0x6041,0 /* Status word for Motor controller */
+#  define ECAT_CTL_STATUS_READY             (1<<0)
+#  define ECAT_CTL_STATUS_ON                (1<<1)
+#  define ECAT_CTL_STATUS_OP_ENABLED        (1<<2)
+#  define ECAT_CTL_STATUS_FAULT             (1<<3)
+#  define ECAT_CTL_STATUS_VOLTAGE_ENABLED   (1<<4)
+#  define ECAT_CTL_STATUS_QUICK_STOP        (1<<5)
+#  define ECAT_CTL_STATUS_DISABLED          (1<<6)
+#  define ECAT_CTL_STATUS_WARNING           (1<<7)
+#  define ECAT_CTL_STATUS_LAST_ABORTED      (1<<8)
+#  define ECAT_CTL_STATUS_USING_CANOPEN     (1<<9)
+#  define ECAT_CTL_STATUS_TARGET_REACHED    (1<<10)
+#  define ECAT_CTL_STATUS_LIMIT             (1<<11)
+#  define ECAT_CTL_STATUS_MOVING            (1<<14)
+#  define ECAT_CTL_STATUS_HOME_CAP          (1<<15)
+
+#define ECAT_CTL_WORD 0x6040,0 /* Control work for motor controller */
+#  define ECAT_CTL_ON                       (1<<0)
+#  define ECAT_CTL_ENABLE_VOLTAGE           (1<<1)
+#  define ECAT_CTL_QUICK_STOP               (1<<2) /* Clear to enable quick stop */
+#  define ECAT_CTL_ENABLE                   (1<<3)
+#  define ECAT_CTL_RESET_FAULT              (1<<7)
+#  define ECAT_CTL_HALT                     (1<<8)
 #endif /* INCLUDE_EC_MOTORS_H_ */
--- mcp/motors/ec_motors.c
+++ mcp/motors/ec_motors.c
@@ -50,7 +50,10 @@ static pthread_t motor_ctl_id;
 // device node Serial Numbers
 #define RW_SN 0x01bbbb5b
 #define PIV_SN 0x02924687
-#define EL_SN 0x01234
+#define EL_SN 0x01238408
+#define RW_ADDR 0x1001
+#define EL_ADDR 0x1002
+#define PIV_ADDR 0x1003
 
 /**
  * Index numbers for the slave array.  0 is the master (flight computer)
@@ -62,7 +65,7 @@ static int el_index = 0;
 /**
  * Ethercat driver status
  */
-static ec_motor_state_t controller_state = EC_MOTOR_COLD;
+static ec_motor_state_t controller_state = ECAT_MOTOR_COLD;
 
 /**
  * Memory mapping for the PDO variables
@@ -70,6 +73,198 @@ static ec_motor_state_t controller_state = EC_MOTOR_COLD;
 static char io_map[4096];
 
 static int motors_exit = false;
+
+#define N_MCs 3
+
+/**
+ * The following pointers reference data points read from/written to
+ * PDOs for each motor controller.  Modifying the data at the address for the write
+ * word will cause that change to be transmitted to the motor controller at
+ * the next PDO cycle (0.5ms)
+ */
+/// Read words
+static int32_t *motor_position[N_MCs] = {NULL};
+static int32_t *motor_velocity[N_MCs] = NULL;
+static int16_t *motor_torque[N_MCs] = {NULL};
+static int16_t *motor_current[N_MCs] = {NULL};
+static uint32_t *status_register[N_MCs] = {NULL};
+static int16_t *amp_temp[N_MCs] = {NULL};
+static int16_t *status_word[N_MCs] = {NULL};
+
+/// Write words
+static uint16_t *control_word[N_MCs] = {NULL};
+static int16_t *target_torque[N_MCs] = {NULL};
+
+/**
+ * This set of functions return the absolute position read by each motor controller
+ * @return int32 value of the position (modulo the wrap position value)
+ */
+int32_t rw_get_position(void)
+{
+    return motor_position[rw_index];
+}
+int32_t el_get_position(void)
+{
+    return motor_position[el_index];
+}
+int32_t piv_get_position(void)
+{
+    return motor_position[piv_index];
+}
+
+/**
+ * This set of functions return the calculated motor velocity of each motor
+ * controller
+ * @return int32 value of the velocity in counts per second
+ */
+int32_t rw_get_velocity(void)
+{
+    return motor_velocity[rw_index];
+}
+int32_t el_get_velocity(void)
+{
+    return motor_velocity[el_index];
+}
+int32_t piv_get_velocity(void)
+{
+    return motor_velocity[piv_index];
+}
+
+/**
+ * This set of functions return the torque in fractions of the max rated
+ * torque
+ * @return int16 fraction of the maximum rated torque
+ */
+int16_t rw_get_torque(void)
+{
+    return motor_torque[rw_index];
+}
+int16_t el_get_torque(void)
+{
+    return motor_torque[el_index];
+}
+int16_t piv_get_torque(void)
+{
+    return motor_torque[piv_index];
+}
+
+/**
+ * This set of functions returns the current in units of 0.01A
+ * @return int16 value of the current
+ */
+int16_t rw_get_current(void)
+{
+    return motor_current[rw_index];
+}
+int16_t el_get_current(void)
+{
+    return motor_current[el_index];
+}
+int16_t piv_get_current(void)
+{
+    return motor_current[piv_index];
+}
+
+/**
+ * Returns a bitmap of what the motor controller is currently doing
+ * @return int16 bitmap
+ */
+int16_t rw_get_status_word(void)
+{
+    return status_word[rw_index];
+}
+int16_t el_get_status_word(void)
+{
+    return status_word[el_index];
+}
+int16_t piv_get_status_word(void)
+{
+    return status_word[piv_index];
+}
+
+/**
+ * Returns a bitmap of the state of the motor controller
+ * @return int32 bitmap
+ */
+uint16_t rw_get_status_register(void)
+{
+    return status_register[rw_index];
+}
+uint16_t el_get_status_register(void)
+{
+    return status_register[el_index];
+}
+uint16_t piv_get_status_register(void)
+{
+    return status_register[piv_index];
+}
+
+/**
+ * Gets the current amplifier temperature in units of degrees C
+ * @return int16 degrees C
+ */
+int16_t rw_get_amp_temp(void)
+{
+    return amp_temp[rw_index];
+}
+int16_t el_get_amp_temp(void)
+{
+    return amp_temp[el_index];
+}
+int16_t piv_get_amp_temp(void)
+{
+    return amp_temp[piv_index];
+}
+
+/**
+ * Sets the requested torque for the motor
+ * @param m_torque int16 requested torque in units of rated torque/1000
+ */
+void rw_set_torque(int16_t m_torque)
+{
+    target_torque[rw_index] = m_torque;
+}
+void el_set_torque(int16_t m_torque)
+{
+    target_torque[el_index] = m_torque;
+}
+void piv_set_torque(int16_t m_torque)
+{
+    target_torque[piv_index] = m_torque;
+}
+
+/**
+ * Enables the operation of the motor controller.  These are currently set using the
+ * PDO interface
+ */
+void rw_enable(void)
+{
+    control_word[rw_index] = ECAT_CTL_ON | ECAT_CTL_ENABLE_VOLTAGE | ECAT_CTL_QUICK_STOP| ECAT_CTL_ENABLE;
+}
+void el_enable(void)
+{
+    control_word[el_index] = ECAT_CTL_ON | ECAT_CTL_ENABLE_VOLTAGE | ECAT_CTL_QUICK_STOP| ECAT_CTL_ENABLE;
+}
+void piv_enable(void)
+{
+    control_word[piv_index] = ECAT_CTL_ON | ECAT_CTL_ENABLE_VOLTAGE | ECAT_CTL_QUICK_STOP| ECAT_CTL_ENABLE;
+}
+
+/**
+ * Sets the emergency quick stop flag for each motor
+ */
+void rw_quick_stop(void)
+{
+    control_word[rw_index] &= (~ECAT_CTL_QUICK_STOP);
+}
+void el_quick_stop(void)
+{
+    control_word[el_index] &= (~ECAT_CTL_QUICK_STOP);
+}
+void piv_quick_stop(void)
+{
+    control_word[piv_index] &= (~ECAT_CTL_QUICK_STOP);
+}
 
 /**
  * Finds all motor controllers on the network and sets them to pre-operational state
@@ -81,7 +276,7 @@ static int find_controllers(void)
     int ret_init;
     int ret_config;
 
-    if (controller_state == EC_MOTOR_COLD)
+    if (controller_state == ECAT_MOTOR_COLD)
     {
         if (!(ret_init = ec_init(name))) {
             berror(err, "Could not initialize %s");
@@ -89,19 +284,19 @@ static int find_controllers(void)
         }
     }
 
-    controller_state = EC_MOTOR_INIT;
+    controller_state = ECAT_MOTOR_INIT;
     if (!(ret_config = ec_config(false, &io_map))) {
         berror(err, "No motor controller slaves found on the network!");
         goto find_err;
     }
     bprintf(startup, "ec_config returns %d slaves found", ret_config);
 
-    if (ret_config < 3) controller_state = EC_MOTOR_FOUND_PARTIAL;
-    else controller_state = EC_MOTOR_FOUND;
+    if (ret_config < 3) controller_state = ECAT_MOTOR_FOUND_PARTIAL;
+    else controller_state = ECAT_MOTOR_FOUND;
 
     /* wait for all slaves to reach SAFE_OP state */
     if (ec_statecheck(0, EC_STATE_SAFE_OP, EC_TIMEOUTSTATE * 3) != EC_STATE_SAFE_OP) {
-        controller_state = EC_MOTOR_RUNNING_PARTIAL;
+        controller_state = ECAT_MOTOR_RUNNING_PARTIAL;
         bprintf(err, "Not all slaves reached safe operational state.");
         ec_readstate();
         for (int i = 1; i <= ec_slavecount; i++) {
@@ -112,11 +307,19 @@ static int find_controllers(void)
         }
     }
     else {
-        controller_state = EC_MOTOR_RUNNING;
+        controller_state = ECAT_MOTOR_RUNNING;
     }
 
     for (int i = 1; i <= ec_slavecount; i++) {
         bprintf(startup, "Motor Controller %d: %s", i, ec_slave[i].name);
+
+        /**
+         * Configure the index values for later use.  These are mapped to the hard-set
+         * addresses on the motor controllers (look for the dials on the side)
+         */
+        if (ec_slave[i].configadr == RW_ADDR) rw_index = i;
+        if (ec_slave[i].configadr == PIV_ADDR) piv_index = i;
+        if (ec_slave[i].configadr == EL_ADDR) el_index = i;
     }
     return ec_slavecount;
 
@@ -124,6 +327,17 @@ find_err:
     return -1;
 }
 
+/**
+ * Configure the PDO assignments.  PDO (Process Data Objects) are sent on configured SYNC cycles
+ * as a static block of up to 8 bytes each.  Locally, we map these objects to a chunk of memory
+ * that can be read or modified outside of the loop that send these objects to the controllers.
+ *
+ * In general, we save these parameters to the FLASH of the controller before flight but this function
+ * acts to ensure a correct mapping upon startup, should the flash be corrupted.
+ *
+ * @param m_slave Slave index number to configure.
+ * @return -1 on failure otherwise 0
+ */
 static int motor_pdo_init(int m_slave)
 {
     int len;
@@ -140,8 +354,8 @@ static int motor_pdo_init(int m_slave)
      * To program the PDO mapping, we first must clear the old state
      */
 
-    ec_SDOwrite8(m_slave, EC_TXPDO_ASSIGNMENT, 0, 0);
-    for (int i = 0; i < 4; i++) ec_SDOwrite8(m_slave, EC_TXPDO_MAPPING + i, 0, 0);
+    ec_SDOwrite8(m_slave, ECAT_TXPDO_ASSIGNMENT, 0, 0);
+    for (int i = 0; i < 4; i++) ec_SDOwrite8(m_slave, ECAT_TXPDO_MAPPING + i, 0, 0);
 
     /**
      * Define the PDOs that we want to send to the flight computer from the Controllers
@@ -149,14 +363,14 @@ static int motor_pdo_init(int m_slave)
     map.index = 0x6063; // Actual position (counts)
     map.subindex = 0;
     map.size = 32;
-    ec_SDOwrite32(m_slave, EC_TXPDO_MAPPING, 1, map.val);
+    ec_SDOwrite32(m_slave, ECAT_TXPDO_MAPPING, 1, map.val);
 
     map.index = 0x6069; // Actual velocity (0.1 counts/sec)
     map.subindex = 0;
     map.size = 32;
-    ec_SDOwrite32(m_slave, EC_TXPDO_MAPPING, 2, map.val);
-    ec_SDOwrite8(m_slave, EC_TXPDO_MAPPING, 0, 2); /// Set the 0x1a00 map to contain 2 elements
-    ec_SDOwrite16(m_slave, EC_TXPDO_ASSIGNMENT, 1, EC_TXPDO_MAPPING); /// Set the 0x1a00 map to the first PDO
+    ec_SDOwrite32(m_slave, ECAT_TXPDO_MAPPING, 2, map.val);
+    ec_SDOwrite8(m_slave, ECAT_TXPDO_MAPPING, 0, 2); /// Set the 0x1a00 map to contain 2 elements
+    ec_SDOwrite16(m_slave, ECAT_TXPDO_ASSIGNMENT, 1, ECAT_TXPDO_MAPPING); /// Set the 0x1a00 map to the first PDO
 
     /**
      * Second map (0x1a01 register)
@@ -164,14 +378,14 @@ static int motor_pdo_init(int m_slave)
     map.index = 0x6077; // Actual torque (rated torque/1000)
     map.subindex = 0;
     map.size = 16;
-    ec_SDOwrite32(m_slave, EC_TXPDO_MAPPING+1, 1, map.val);
+    ec_SDOwrite32(m_slave, ECAT_TXPDO_MAPPING+1, 1, map.val);
 
     map.index = 0x6040; // Actual current (0.01A)
     map.subindex = 0;
     map.size = 16;
-    ec_SDOwrite32(m_slave, EC_TXPDO_MAPPING+1, 2, map.val);
-    ec_SDOwrite8(m_slave, EC_TXPDO_MAPPING+1, 0, 2); /// Set the 0x1a01 map to contain 2 elements
-    ec_SDOwrite16(m_slave, EC_TXPDO_ASSIGNMENT+1, 2, EC_TXPDO_MAPPING); /// Set the 0x1a01 map to the second PDO
+    ec_SDOwrite32(m_slave, ECAT_TXPDO_MAPPING+1, 2, map.val);
+    ec_SDOwrite8(m_slave, ECAT_TXPDO_MAPPING+1, 0, 2); /// Set the 0x1a01 map to contain 2 elements
+    ec_SDOwrite16(m_slave, ECAT_TXPDO_ASSIGNMENT+1, 2, ECAT_TXPDO_MAPPING); /// Set the 0x1a01 map to the second PDO
 
     /**
      * Third map (0x1a02 register)
@@ -179,28 +393,28 @@ static int motor_pdo_init(int m_slave)
     map.index = 0x1002; // Status Register
     map.subindex = 0;
     map.size = 32;
-    ec_SDOwrite32(m_slave, EC_TXPDO_MAPPING+2, 1, map.val);
+    ec_SDOwrite32(m_slave, ECAT_TXPDO_MAPPING+2, 1, map.val);
 
     map.index = 0x6041; // Status Word
     map.subindex = 0;
     map.size = 16;
-    ec_SDOwrite32(m_slave, EC_TXPDO_MAPPING+2, 2, map.val);
+    ec_SDOwrite32(m_slave, ECAT_TXPDO_MAPPING+2, 2, map.val);
 
     map.index = 0x6041; // Amplifier Temp (deg C)
     map.subindex = 0;
     map.size = 16;
-    ec_SDOwrite32(m_slave, EC_TXPDO_MAPPING+2, 3, map.val);
-    ec_SDOwrite8(m_slave, EC_TXPDO_MAPPING+2, 0, 3); /// Set the 0x1a01 map to contain 3 elements
-    ec_SDOwrite16(m_slave, EC_TXPDO_ASSIGNMENT+2, 3, EC_TXPDO_MAPPING); /// Set the 0x1a02 map to the third PDO
+    ec_SDOwrite32(m_slave, ECAT_TXPDO_MAPPING+2, 3, map.val);
+    ec_SDOwrite8(m_slave, ECAT_TXPDO_MAPPING+2, 0, 3); /// Set the 0x1a01 map to contain 3 elements
+    ec_SDOwrite16(m_slave, ECAT_TXPDO_ASSIGNMENT+2, 3, ECAT_TXPDO_MAPPING); /// Set the 0x1a02 map to the third PDO
 
-    ec_SDOwrite8(m_slave, EC_TXPDO_ASSIGNMENT, 0, 3); /// There are three maps in the TX PDOs
+    ec_SDOwrite8(m_slave, ECAT_TXPDO_ASSIGNMENT, 0, 3); /// There are three maps in the TX PDOs
 
 
     /**
      * To program the PDO mapping, we first must clear the old state
      */
-    ec_SDOwrite8(m_slave, EC_RXPDO_ASSIGNMENT, 0, 0);
-    for (int i = 0; i < 4; i++) ec_SDOwrite8(m_slave, EC_RXPDO_MAPPING + i, 0, 0);
+    ec_SDOwrite8(m_slave, ECAT_RXPDO_ASSIGNMENT, 0, 0);
+    for (int i = 0; i < 4; i++) ec_SDOwrite8(m_slave, ECAT_RXPDO_MAPPING + i, 0, 0);
     /**
      * Define the PDOs that we want to send from the flight computer to the Controllers
      */
@@ -208,14 +422,14 @@ static int motor_pdo_init(int m_slave)
     map.index = 0x6040; // Control Word
     map.subindex = 0;
     map.size = 16;
-    ec_SDOwrite32(m_slave, EC_RXPDO_MAPPING, 1, map.val);
+    ec_SDOwrite32(m_slave, ECAT_RXPDO_MAPPING, 1, map.val);
 
     map.index = 0x6071; // Target Torque
     map.subindex = 0;
     map.size = 16;
-    ec_SDOwrite32(m_slave, EC_RXPDO_MAPPING, 2, map.val);
-    ec_SDOwrite8(m_slave, EC_RXPDO_MAPPING, 0, 2); /// Set the 0x1600 map to contain 2 elements
-    ec_SDOwrite16(m_slave, EC_RXPDO_ASSIGNMENT, 1, EC_RXPDO_MAPPING); /// Set the 0x1600 map to the first PDO
+    ec_SDOwrite32(m_slave, ECAT_RXPDO_MAPPING, 2, map.val);
+    ec_SDOwrite8(m_slave, ECAT_RXPDO_MAPPING, 0, 2); /// Set the 0x1600 map to contain 2 elements
+    ec_SDOwrite16(m_slave, ECAT_RXPDO_ASSIGNMENT, 1, ECAT_RXPDO_MAPPING); /// Set the 0x1600 map to the first PDO
 
     /**
      * Get the current value of each RX word to avoid stomping on the current state
@@ -227,15 +441,21 @@ static int motor_pdo_init(int m_slave)
     return 0;
 }
 
+/**
+ * There must be only a single distributed clock master on the entire bus.  This should be one of the
+ * controllers and so we choose the first slave that has dc enabled as the SYNC master.  Everyone
+ * else on the bus gets the same DC_CYCLE (in nanoseconds) but have their sync disabled.
+ */
 static void motor_configure_timing(void)
 {
     int found_dc_master = 0;
+    ec_configdc();
     for (int i = 1; i <= ec_slavecount; i++) {
         if (!found_dc_master && ec_slave[i].hasdc) {
-            ec_dcsync0(i, true, EC_DC_CYCLE_NS, ec_slave[i].pdelay);
+            ec_dcsync0(i, true, ECAT_DC_CYCLE_NS, ec_slave[i].pdelay);
         }
         else {
-            ec_dcsync0(i, false, EC_DC_CYCLE_NS, ec_slave[i].pdelay);
+            ec_dcsync0(i, false, ECAT_DC_CYCLE_NS, ec_slave[i].pdelay);
         }
     }
 }
@@ -266,7 +486,7 @@ static int motor_set_operational()
      * If we've reached fully operational state, return
      */
     if (ec_slave[0].state == EC_STATE_OPERATIONAL) {
-        controller_state = EC_MOTOR_RUNNING;
+        controller_state = ECAT_MOTOR_RUNNING;
         return 0;
     }
 
@@ -284,10 +504,9 @@ static int motor_set_operational()
 
 static void* motor_control(void* arg)
 {
-    find_controllers();
+    int expectedWKC, wkc;
 
-    /* configure DC options for every DC capable slave found in the list */
-    bprintf(startup, "DC capable Motor Controllers : %d\n",ec_configdc());
+    find_controllers();
 
     for (int i = 1; i <= ec_slavecount; i++) {
         motor_pdo_init(i);
@@ -301,8 +520,12 @@ static void* motor_control(void* arg)
     /// Put the motors in Operational mode (EtherCAT Operation)
     motor_set_operational();
 
+    /// Our work counter (WKC) provides a count of the number of items to handle.
+    expectedWKC = (ec_group[0].outputsWKC * 2) + ec_group[0].inputsWKC;
     while (!motors_exit) {
 
+        ec_send_processdata();
+        wkc = ec_receive_processdata(EC_TIMEOUTRET);
     }
 }
 
