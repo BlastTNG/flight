badf7519d313976ce689a9c02c2f62f3a6be94d8
Seth Hillbrand <seth.hillbrand@gmail.com>
2015-10-26 16:32:47 -0700

    Add todo to check scaling in outputs/commands

--- blast_config/tx_struct_tng.c
+++ blast_config/tx_struct_tng.c
@@ -789,7 +789,7 @@ channel_t channel_list[] =
     {"network_status_rw",        1.0,    0.0, TYPE_UINT16, RATE_5HZ, SRC_FC, U_NONE, 0},
     {"network_status_piv",        1.0,    0.0, TYPE_UINT16, RATE_5HZ, SRC_FC, U_NONE, 0},
 
-
+    ///TODO: Ensure that scale factors for all currents/commands are in Amps
     { "mc_piv_i_cmd",           1.0/100.0,  0.0, TYPE_INT16, RATE_200HZ, SRC_FC, U_NONE, 0 },
     { "mc_rw_i_cmd",            1.0/100.0,  0.0, TYPE_INT16, RATE_200HZ, SRC_FC, U_NONE, 0 },
     { "mc_el_i_cmd",            1.0/100.0,  0.0, TYPE_INT16, RATE_200HZ, SRC_FC, U_NONE, 0 },
