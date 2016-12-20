346ec0c2a3cc4d81901254223bfbbcecf1164f3e
Joy Didier <didier.joy@gmail.com>
2016-12-20 15:48:26 -0800

    settings the bits to output before writing out

--- test_programs/biphase/test_biphase.c
+++ test_programs/biphase/test_biphase.c
@@ -81,6 +81,7 @@ void send_biphase_writes() {
         uint32_t offset = 0;
         //uint32_t bytes_to_write = 1535; //
         uint32_t bytes_to_write = 625; // That is the correct rate for code running at 100 Hz to write at 1 Mbps
+        //uint32_t bytes_to_write = 32; 
         //uint32_t bytes_to_write = 16000; //
         unsigned size_of_frame = bytes_to_write; //8192; // in bytes
         int frequency = 1000000;
@@ -88,10 +89,16 @@ void send_biphase_writes() {
         struct timeval begin, end;
 
 
+        uint8_t data = 0x00;
+        uint8_t dir = 0xFF;  // direction output for all bits
+        //uint8_t dir = 0x00;  // direction input for all bits
+
+        mpsse_set_data_bits_low_byte(ctx, data, dir);
+        mpsse_set_data_bits_high_byte(ctx, data, dir);
         data_to_write = malloc(size_of_frame); 
         if (data_to_write) {
             for (int i = 0; i < size_of_frame; i++) {
-                *(data_to_write+i) = 0x00;
+                *(data_to_write+i) = 0xF0;
             }
         } else {
            mpsse_close(ctx); 
@@ -99,11 +106,15 @@ void send_biphase_writes() {
 
         //mpsse_rtck_config(ctx, false);
         mpsse_set_frequency(ctx, frequency);
+        mpsse_flush(ctx);
+        usleep(1000);
 
+        //while(i <= 1) {
         while(true) {
             gettimeofday(&begin, NULL);
             blast_dbg("I am about to call mpsse_biphase_write_data, i=%d", i);
             mpsse_biphase_write_data(ctx, data_to_write, offset, bytes_to_write);
+            //mpsse_clock_data_out(ctx, data_to_write, offset, bytes_to_write*8, NEG_EDGE_OUT | MSB_FIRST);
             blast_dbg("Flushing the buffer");
             mpsse_flush(ctx);
             i += 1;
@@ -113,7 +124,7 @@ void send_biphase_writes() {
             //    i = 0;
             //}
             //usleep(4096); //4096 to be exact, because it takes 4096 us to send 512 bytes at 1 Mb/s
-            usleep(10000); // should write 1.25 KB per 0.01 second at 1 MBps
+            //usleep(10000); // should write 1.25 KB per 0.01 second at 1 MBps
             gettimeofday(&end, NULL);
             blast_dbg("It took %f second to run", (end.tv_usec - begin.tv_usec)/1000000.0);
         } 
