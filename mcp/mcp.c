5821c7e8c34cb4306dcbcce59bdacce827bdd8cb
Laura Fissel <laura.fissel@northwestern.edu>
2017-05-11 21:44:46 -0400

    Commenting out the 1hz thermometry functions which seem to cause mcp to hang.

--- mcp/mcp.c
+++ mcp/mcp.c
@@ -387,12 +387,12 @@ static void mcp_2hz_routines(void)
 }
 static void mcp_1hz_routines(void)
 {
-    rec_control();
+    // rec_control();
     // of_control();
     // if_control();
     // heater_control();
     // test_labjacks(0);
-    read_thermometers();
+    // read_thermometers();
     // test_read();
     blast_store_cpu_health();
     blast_store_disk_space();
