b3dc9f4751fbd76858893e22cc8ced315edd16ea
Sam Gordon <sbg2133@gmail.com>
2017-07-14 15:20:33 -0400

    Merged master, CPU usage down to 26% without Roaches enabled

--- mcp/mcp.c
+++ mcp/mcp.c
@@ -585,8 +585,8 @@ int main(int argc, char *argv[])
 
 #ifndef NO_KIDS_TEST
 blast_info("Initializing ROACHes from MCP...");
-// roach_udp_networking_init();
-// init_roach(0);
+roach_udp_networking_init();
+init_roach(0);
 // init_roach(1);
 // init_roach(2);
 // init_roach(3);
