f71ccdf63287e5dc522d91c89c1575c615f5a10b
Sam Gordon <sbg2133@gmail.com>
2017-07-31 18:10:08 -0700

    Merged with Roach branch, issue with store_roach_udp_packet() in roach_udp.c
    Conflicts:
    	mcp/roach/roach_udp.c

--- mcp/roach/roach_udp.c
+++ mcp/roach/roach_udp.c
@@ -236,15 +236,10 @@ void roach_process_stream(roach_handle_data_t *m_roach_udp, data_udp_packet_t *m
 {
     parse_udp_packet(m_packet);
     uint16_t udperr = check_udp_packet(m_packet, m_roach_udp);
-    // store_roach_udp_packet(&m_packet, m_roach_udp); // Writes packet to harddrive.
-    /* if (udperr > 0) {
-        blast_info("UDPERR = %d", udperr);
-        // continue;
-        udp_store_to_structure(m_roach_udp, m_packet);
-        m_roach_udp->have_warned = 0;
-    }*/
-        udp_store_to_structure(m_roach_udp, m_packet);
-        m_roach_udp->have_warned = 0;
+    // store_roach_udp_packet(m_packet, m_roach_udp, udperr); // Writes packet to harddrive.
+    if (udperr > 0) return;
+    udp_store_to_structure(m_roach_udp, m_packet);
+    m_roach_udp->have_warned = 0;
 }
 
 /* Function: poll_socket
