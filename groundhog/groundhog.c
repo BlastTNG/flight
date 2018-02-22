617794aff3a9cb95726c8157ecb11aacf5b4dcce
Sam Gordon <sbg2133@gmail.com>
2018-02-22 12:53:13 -0500

    more including tdrss stuff

--- groundhog/groundhog.c
+++ groundhog/groundhog.c
@@ -126,22 +126,28 @@ int main(int argc, char * argv[]) {
   // Receiving data from telemetry
   pthread_t pilot_receive_worker;
   pthread_t biphase_receive_worker;
+  // pthread_t tdrss_receive_worker;
 
   pthread_create(&pilot_receive_worker, NULL, (void *) &pilot_receive, NULL);
   pthread_create(&biphase_receive_worker, NULL, (void *) &biphase_receive, NULL);
+  // pthread_create(&tdrss_receive_worker, NULL, (void *) &biphase_receive, NULL);
 
   // Publishing data to MSQT
   pthread_t pilot_publish_worker;
   pthread_t biphase_publish_worker;
+  // pthread_t tdrss_publish_worker;
 
   pthread_create(&pilot_publish_worker, NULL, (void *) &pilot_publish, NULL);
   pthread_create(&biphase_publish_worker, NULL, (void *) &biphase_publish, NULL);
+  // pthread_create(&tdrss_publish_worker, NULL, (void *) &biphase_publish, NULL);
 
   // Joining
   pthread_join(pilot_receive_worker, NULL);
   pthread_join(biphase_receive_worker, NULL);
+  // pthread_join(tdrss_receive_worker, NULL);
   pthread_join(pilot_publish_worker, NULL);
   pthread_join(biphase_publish_worker, NULL);
+  // pthread_join(tdrss_publish_worker, NULL);
 
   return 0;
 }
