7f616b300555627efe01b9691b2fa66763c9bbbc
javier <javier.romualdez@gmail.com>
2018-03-19 18:54:02 -0400

    More bi0los tests

--- groundhog/groundhog.h
+++ groundhog/groundhog.h
@@ -3,6 +3,14 @@
 
 #define FILE_SAVE_DIR "/data/groundhog"
 
+struct UDPSetup {
+  char name[80];
+  char addr[80];
+  unsigned int port;
+  unsigned int maxsize;
+  unsigned int packetsize;
+};
+
 void pilot_receive(void *arg);
 void pilot_publish(void *arg);
 void biphase_receive(void *arg);
