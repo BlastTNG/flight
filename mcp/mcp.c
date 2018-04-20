282cd3fb04379fd359e69227045c79391c3d3a9e
Ian Lowe <ian@Ians-MacBook-Air.local>
2018-04-20 12:51:38 -0400

    set up to run all labjacks from the gondola for sam testing

--- mcp/mcp.c
+++ mcp/mcp.c
@@ -290,17 +290,17 @@ void * lj_connection_handler(void *arg) {
     // last argument turns commanding on/off
     // arguments are 1/0 0 off 1 on
     // order is CRYO1 CRYO2 OF1 OF2 OF3
-    init_labjacks(1, 1, 0, 0, 0, 1);
-    // mult_labjack_networking_init(LABJACK_MULT_OF, 84, 1);
+    init_labjacks(1, 1, 1, 1, 1, 1);
+    mult_labjack_networking_init(LABJACK_MULT_OF, 84, 1);
     // 7 is for highbay labjack
     labjack_networking_init(7, 14, 1);
-    ph_thread_t *cmd_thread = initialize_labjack_commands(7);
+    initialize_labjack_commands(7);
     // initializes an array of voltages for load curves
     init_array();
     // labjack_networking_init(8, 14, 1);
     // initialize_labjack_commands(8);
     // switch to this thread for flight
-    // ph_thread_t *cmd_thread = mult_initialize_labjack_commands(6);
+    ph_thread_t *cmd_thread = mult_initialize_labjack_commands(6);
     ph_thread_join(cmd_thread, NULL);
 
     return NULL;
@@ -415,7 +415,7 @@ static void mcp_1hz_routines(void)
     // out frame monitoring (current loops and thermistors) 1 on 0 off
     outer_frame(1);
     // relays arg defines found in relay.h
-    relays(2);
+    relays(3);
     // highbay will be rewritten as all on or off when box is complete
     highbay(1);
     // thermal_vac();
@@ -648,7 +648,7 @@ int main(int argc, char *argv[])
   initialize_CPU_sensors();
 
   // force incharge for test cryo
-  force_incharge();
+  // force_incharge();
 
   if (use_starcams) {
       xsc_networking_init(0);
