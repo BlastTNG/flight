77421e7cc7f29034fc08f1500961c30a7aacc60b
javier <javier.romualdez@gmail.com>
2018-05-05 23:59:20 -0400

    Fixed upload_linklist script

--- upload_linklists.sh
+++ upload_linklists.sh
@@ -1,14 +1,7 @@
 #! /bin/bash
 
-<<<<<<< HEAD
-sudo rsync -avz --ignore-existing --delete blast_config/linklists/ /data/etc/linklists/
-rsync -avz --ignore-existing --delete blast_config/linklists/ fc1:/data/etc/linklists/
-rsync -avz --ignore-existing --delete blast_config/linklists/ fc2:/data/etc/linklists/
+sudo rsync -avz --delete blast_config/linklists/ /data/etc/linklists/
+rsync -avz --delete blast_config/linklists/ fc1:/data/etc/linklists/
+rsync -avz --delete blast_config/linklists/ fc2:/data/etc/linklists/
 #rsync -avz --delete blast_config/linklists fc1user@fc1mustang:/data/etc/
-=======
-sudo rsync -rtvu --delete blast_config/linklists /data/etc/
-rsync -rtvu --delete blast_config/linklists fc1:/data/etc/
-rsync -rtvu --delete blast_config/linklists fc2:/data/etc/
-#rsync -rtvu --delete blast_config/linklists fc1user@fc1mustang:/data/etc/
->>>>>>> master
 
