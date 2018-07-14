3acf3feeb6a83ded02acc4fcde6a7fb03329ffb0
javier <javier.romualdez@gmail.com>
2018-07-14 13:42:45 -0500

    Added a pull to the restart script for new branchees

--- restart_blastgs.sh
+++ restart_blastgs.sh
@@ -11,7 +11,7 @@ else
   GIT_BRANCH="$2"
 fi
 
-GIT_PULL_CMD="cd $REPO_DIR; git checkout $GIT_BRANCH; git pull origin $GIT_BRANCH"
+GIT_PULL_CMD="cd $REPO_DIR; git pull; git checkout $GIT_BRANCH; git pull origin $GIT_BRANCH"
 
 GROUNDHOG_BUILD_CMD="cd $GROUNDHOG_BUILD_DIR; cmake ../; make clean all; sudo make install"
 GROUNDHOG_RESTART_CMD="sudo pkill groundhog"
