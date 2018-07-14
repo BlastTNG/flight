3efaac5362a3010b650b502e7d8c9dc9b44c287a
javier <javier.romualdez@gmail.com>
2018-07-14 16:57:43 -0500

    mcp install script now checks and start the inf loop

--- install_and_restart_mcp.sh
+++ install_and_restart_mcp.sh
@@ -3,12 +3,12 @@
 TMP_DIR="/tmp"
 INSTALL_DIR="/usr/local/bin"
 
-INF_LOOP_CHECK="if pidof blast_inf_loop > /dev/null 2>&1; then echo 'Inf loop already running'; else echo 'Starting inf loop'; sudo bash -c '/usr/local/bin/blast_inf_loop >/dev/null 2>&1 &' fi"
-KILL_MCP="sudo kill -INT \$(pidof mcp)"
+INF_LOOP_CHECK="cd; sudo pkill blast_inf_loop; sudo nohup ./start_blast_inf_loop > /dev/null 2> /dev/null < /dev/null"
+KILL_MCP="sudo kill -INT \$(pidof mcp) > /dev/null 2>&1"
 INSTALL_MCP="install -m 755 -p $TMP_DIR/mcp $INSTALL_DIR/" 
 
 scp mcp fc1user@fc1:$TMP_DIR/ 
 scp mcp fc1user@fc2:$TMP_DIR/
 
-ssh -t fc1user@fc1 "$INSTALL_MCP; $KILL_MCP"
-ssh -t fc1user@fc2 "$INSTALL_MCP; $KILL_MCP"
+ssh -t fc1user@fc1 "$INF_LOOP_CHECK; $INSTALL_MCP; $KILL_MCP"
+ssh -t fc1user@fc2 "$INF_LOOP_CHECK; $INSTALL_MCP; $KILL_MCP"
