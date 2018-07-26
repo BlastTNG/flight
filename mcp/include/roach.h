b7da2e80598ff10bc073928a636e66cf7ec364a8
Laura Fissel <laura.fissel@northwestern.edu>
2018-07-25 19:53:53 -0500

    Added new structures for the fast roach df calculation.

--- mcp/include/roach.h
+++ mcp/include/roach.h
@@ -36,6 +36,7 @@
 #include "phenom/buffer.h"
 #include "remote_serial.h"
 #include "linklist.h"
+#include "channels_tng.h"
 
 #define ROACH_UDP_CRC_ERR 0x01
 #define ROACH_UDP_SEQ_ERR 0x02
@@ -44,6 +45,7 @@
 #define NUM_ROACHES 5
 #define NUM_ROACH_UDP_CHANNELS 1024
 #define ROACH_UDP_LEN 8234
+#define ROACH_DF_FILT_LEN 10
 #define ROACH_UDP_DATA_LEN NUM_ROACH_UDP_CHANNELS * 4 * 2
 #define IPv4(a, b, c, d) ((uint32_t)(((a) & 0xff) << 24) | \
                                             (((b) & 0xff) << 16) | \
@@ -256,6 +258,16 @@ typedef struct {
     int sock; // the socket file descriptor
     struct timeval  timeout;
 } roach_handle_data_t;
+
+typedef struct {
+    float ibuf[ROACH_DF_FILT_LEN];
+    float qbuf[ROACH_DF_FILT_LEN];
+    int i_last;
+    float i_avg;
+    float q_avg;
+} roach_df_calc_t;
+
+roach_df_calc_t roach_df_telem[NUM_ROACH_TLM];
 
 roach_handle_data_t roach_udp[NUM_ROACHES];
 
