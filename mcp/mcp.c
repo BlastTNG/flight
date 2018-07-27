04a8768aa2dda712ac85e61dd33ed5ebce724b85
Laura Fissel <laura.fissel@northwestern.edu>
2018-07-27 16:17:36 -0500

    Changed the outer frame current sensor and pss filtering.

--- mcp/mcp.c
+++ mcp/mcp.c
@@ -397,6 +397,8 @@ static void mcp_244hz_routines(void)
 
 static void mcp_200hz_routines(void)
 {
+    outer_frame_200hz(1);
+    process_sun_sensors();
     store_200hz_acs();
     command_motors();
     write_motor_channels_200hz();
@@ -485,7 +487,7 @@ static void mcp_1hz_routines(void)
     // all 1hz cryo monitoring 1 on 0 off
     cryo_1hz(1);
     // out frame monitoring (current loops and thermistors) 1 on 0 off
-    outer_frame(1);
+    outer_frame_1hz(1);
     update_mult_vac();
     // relays arg defines found in relay.h
     relays(3);
