aecf7065867815df239f6c2baf9a7819ccebadf2
javier <javier.romualdez@gmail.com>
2018-08-11 09:25:38 -0500

    Improvements to linklist handling of rawfiles and dirfiles; upstreamed changes to mole and groundhog

--- linklist_writer.h
+++ linklist_writer.h
@@ -31,19 +31,24 @@ typedef struct linklist_rawfile linklist_rawfile_t;
 typedef struct linklist_dirfile_meta linklist_dirfile_meta_t;
 
 int seek_linklist_dirfile(linklist_dirfile_t *, unsigned int);
+int flush_linklist_dirfile(linklist_dirfile_t *);
+
 linklist_dirfile_t * open_linklist_dirfile(char *, linklist_t *);
 linklist_dirfile_t * open_linklist_dirfile_opt(char *, linklist_t *, unsigned int);
 void close_and_free_linklist_dirfile(linklist_dirfile_t *);
 double write_linklist_dirfile(linklist_dirfile_t *, uint8_t *);
+double write_linklist_dirfile_opt(linklist_dirfile_t *, uint8_t *, unsigned int);
 
 int seek_linklist_rawfile(linklist_rawfile_t *, unsigned int);
 int seekend_linklist_rawfile(linklist_rawfile_t *);
+int flush_linklist_rawfile(linklist_rawfile_t *);
 int tell_linklist_rawfile(linklist_rawfile_t *);
 
 linklist_rawfile_t * open_linklist_rawfile(char *, linklist_t *);
 linklist_rawfile_t * open_linklist_rawfile_opt(char *, linklist_t *, unsigned int);
 void close_and_free_linklist_rawfile(linklist_rawfile_t *);
 int write_linklist_rawfile(linklist_rawfile_t *, uint8_t *);
+int write_linklist_rawfile_opt(linklist_rawfile_t *, uint8_t *, unsigned int);
 int read_linklist_rawfile(linklist_rawfile_t *, uint8_t *);
 
 void make_linklist_rawfile_name(linklist_t *, char *);
