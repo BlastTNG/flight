aaa5e0865e38a243584f0a26f28bea2f0bee7f13
javier <javier.romualdez@gmail.com>
2018-12-13 12:41:28 +1300

    Added sonos monitoring python script

---
+++ scripts_gse/sonos_mon.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+import time
+import numpy as np
+from datetime import datetime
+import pygetdata as gd
+import soco
+
+DEVICE_ADDRESS = "192.168.0.135"
+DATAFILE = "/data/etc/mole.lnk"
+REF_FIELD = "MCP_1HZ_FRAMECOUNT"
+
+LUNCH_SONG = "Mmm Mmm"
+
+sonos = soco.SoCo(DEVICE_ADDRESS)
+df = gd.dirfile(DATAFILE, gd.RDONLY)
+
+# find the tracks
+
+def get_current_track_title():
+  return sonos.get_current_track_info()['title']
+
+class AutoSonos:
+  def __init__(self, trackname, fieldname, trueval=None):
+    self.trackname = trackname
+    self.fieldname = fieldname
+    self.trueval = trueval
+    self.timeout = 0
+    self.lastval = 0
+    self.framenum = 0
+    self.changed = False
+
+    self.update()
+    self.track = sonos.music_library.get_tracks(search_term=self.trackname)[0]
+
+  # Checks to see if there is new data in the dirfile
+  # Increments a timeout when there is no new data
+  def has_update(self):
+    num = df.eof(self.fieldname)
+    retval = False
+    if (num != self.framenum):
+      self.timeout = 0
+      retval = True
+    else:
+      self.timeout += 1
+    self.framenum = num
+    return retval
+
+  # General command to be called always in the loop
+  # Loads data from dirfile if necessary
+  def update(self):
+    if (self.has_update()):
+      val = df.getdata(self.fieldname,
+                       first_frame=0,
+                       first_sample=self.framenum-1,
+                       num_frames=0,
+                       num_samples=1)[0]
+      if (val != self.lastval):
+        self.changed = True
+        self.timesteady = 0
+      else:
+        self.changed = False
+        self.timesteady += 1
+      self.lastval = val  
+
+  # Queues the assigned song
+  # If the song is already playing, doesn't try to play again 
+  def trigger(self):
+    if (get_current_track_title() != self.track.title):
+      sonos.add_to_queue(self.track, 0)
+      sonos.play_from_queue(0, True)
+      print("Playing " + self.track.title)
+      return 1
+    return 0
+
+  # Checks if the value in the dirfile has reached the assigned value
+  def truestate(self):
+    return self.lastval == self.trueval
+
+
+# Looks at field "STATE_POTVALVE" and .truestate() == True when STATE_POTVALVE == 1
+# When the .trigger() function is called, will play "Island Girl" on the SONOS
+PotvalveOpen = AutoSonos("Let my love open the door", "STATE_POTVALVE", 1)
+
+Lunch = AutoSonos("Sandstorm", "TIME")
+
+while True:
+  song_requested = False
+
+  # Potvalve open
+  # Logic is in the main loop
+  PotvalveOpen.update() # must always be called in the loop
+  
+  # If the truestate is reached and has been true for 20 second, then trigger the song
+  if (PotvalveOpen.truestate() and (PotvalveOpen.timesteady >= 20)):
+    print("Potvalve is OPEN!")
+    if not song_requested: 
+      PotvalveOpen.trigger()
+    song_requested = True
+
+  # lunch
+  Lunch.update()
+  date = datetime.utcfromtimestamp(Lunch.lastval)
+  if ((date.hour+13)%24 == 11) and (date.minute >= 45):
+    print("Lunchtime!")
+    if not song_requested: 
+      Lunch.trigger()
+    song_requested = True
+
+  if (Lunch.timeout > 5):
+    df.close()
+    df = gd.dirfile(DATAFILE, gd.RDONLY)
+    print("Reloading \"" + DATAFILE + "\"")
+
+  time.sleep(1)
