d1d2034972313834d7f9623fb22d20d6273e9f87
BLAST ground station user <didier.joy@gmail.com>
2018-12-16 18:35:51 +1300

    Fixed bugs in status field, started on compress_all_data command

--- blast_config/include/command_list.h
+++ blast_config/include/command_list.h
@@ -338,6 +338,7 @@ enum multiCommand {
   set_default_tone_power_all,
   set_attens_default,
   set_find_kids_params,
+  compress_roach_data,
   plugh,                // plugh should be at the end of the list
   sched_packet = 0xff   // not really a command, more of a placeholder
 };
--- mcp/include/command_struct.h
+++ mcp/include/command_struct.h
@@ -389,6 +389,7 @@ typedef struct roach
     unsigned int do_noise_comp;
     unsigned int do_fk_loop;
     unsigned int kill;
+    unsigned int data_type;
 } roach_status_t;
 
 typedef struct roach_params
@@ -495,6 +496,8 @@ struct CommandDataStruct {
   roach_status_t roach[NUM_ROACHES];
   udp_roach_t udp_roach[NUM_ROACHES];
   roach_params_t roach_params[NUM_ROACHES];
+  unsigned int tar_all_data;
+  unsigned int data_type;
   unsigned int trigger_roach_tuning_check; // motors.c sets this flag when a scan is nearly complete
                                    // to (optionally) trigger a retune
 
