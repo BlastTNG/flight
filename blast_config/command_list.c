dffbf64d6dffc36c0fee2b75994078851bfeb17b
javier <javier.romualdez@gmail.com>
2019-12-27 06:30:56 +1300

    Fixed xsc_selective_mask command (from fc2)

--- blast_config/command_list.c
+++ blast_config/command_list.c
@@ -2119,9 +2119,9 @@ struct mcom mcommands[plugh + 2] = {
       {
               {"Which camera (0, 1, 2=both)", 0, 2, 'i', "NONE"},
               {"Enable selective masking", 0, 1, 'i', "NONE"},
-              {"Mask field 1", 0, CMD_L_MAX, 'i', "NONE"},
-              {"Mask field 2", 0, CMD_L_MAX, 'i', "NONE"},
-              {"Mask field 3", 0, CMD_L_MAX, 'i', "NONE"},
+              {"Mask field 1", 0, CMD_L_MAX, 'l', "NONE"},
+              {"Mask field 2", 0, CMD_L_MAX, 'l', "NONE"},
+              {"Mask field 3", 0, CMD_L_MAX, 'l', "NONE"},
       }
   },
   {COMMAND(xsc_blob_finding), "XSC blob finder settings", GR_XSC_PARAM, 5,
